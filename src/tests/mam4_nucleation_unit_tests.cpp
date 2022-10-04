d0478f9b4aff4c0c01c969ab1b0234974e6d11b7
Peter Bosler <pabosle@sandia.gov>
2022-10-04 10:31:06 -0600

    added debug logging to nucleation unit tests

--- src/tests/mam4_nucleation_unit_tests.cpp
+++ src/tests/mam4_nucleation_unit_tests.cpp
@@ -4,6 +4,9 @@
 #include <iostream>
 #include <limits>
 #include <mam4.hpp>
+#include "ekat/logging/ekat_logger.hpp"
+#include "ekat/mpi/ekat_comm.hpp"
+#include <sstream>
 
 using namespace haero;
 
@@ -15,6 +18,10 @@ TEST_CASE("test_constructor", "mam4_nucleation_process") {
 }
 
 TEST_CASE("test_compute_tendencies", "mam4_nucleation_process") {
+  ekat::Comm comm;
+
+  ekat::logger::Logger<> logger("nucleation unit tests", ekat::logger::LogLevel::debug, comm);
+
   int nlev = 72;
   Real pblh = 1000;
   Atmosphere atm(nlev, pblh);
@@ -25,6 +32,26 @@ TEST_CASE("test_compute_tendencies", "mam4_nucleation_process") {
   mam4::AeroConfig mam4_config;
   mam4::NucleationProcess process(mam4_config);
 
+  auto h_prog_qgas0 = Kokkos::create_mirror_view(progs.q_gas[0]);
+  auto h_tend_qgas0 = Kokkos::create_mirror_view(tends.q_gas[0]);
+  Kokkos::deep_copy(h_prog_qgas0, progs.q_gas[0]);
+  Kokkos::deep_copy(h_tend_qgas0, tends.q_gas[0]);
+  std::ostringstream ss;
+  ss << "prog_qgas0 [in]: [ ";
+  for (int k=0; k<nlev; ++k) {
+    ss << h_prog_qgas0(k) << " ";
+  }
+  ss << "]";
+  logger.debug(ss.str());
+  ss.str("");
+  ss << "tend_qgas0 [in]: [ ";
+  for (int k=0; k<nlev; ++k) {
+    ss << h_tend_qgas0(k) << " ";
+  }
+  ss << "]";
+  logger.debug(ss.str());
+  ss.str("");
+
   // Single-column dispatch.
   auto team_policy = ThreadTeamPolicy(1u, Kokkos::AUTO);
   Real t = 0.0, dt = 30.0;
@@ -32,6 +59,23 @@ TEST_CASE("test_compute_tendencies", "mam4_nucleation_process") {
       team_policy, KOKKOS_LAMBDA(const ThreadTeam& team) {
         process.compute_tendencies(team, t, dt, atm, progs, diags, tends);
       });
+
+  Kokkos::deep_copy(h_prog_qgas0, progs.q_gas[0]);
+  Kokkos::deep_copy(h_tend_qgas0, tends.q_gas[0]);
+  ss << "prog_qgas0 [out]: [ ";
+  for (int k=0; k<nlev; ++k) {
+    ss << h_prog_qgas0(k) << " ";
+  }
+  ss << "]";
+  logger.debug(ss.str());
+  ss.str("");
+  ss << "tend_qgas0 [out]: [ ";
+  for (int k=0; k<nlev; ++k) {
+    ss << h_tend_qgas0(k) << " ";
+  }
+  ss << "]";
+  logger.debug(ss.str());
+  ss.str("");
 }
 
 TEST_CASE("test_multicol_compute_tendencies", "mam4_nucleation_process") {
