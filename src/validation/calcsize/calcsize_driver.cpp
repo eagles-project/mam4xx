83868d8b47b2ab64abf876412680af1ebe1f6f51
Oscar <odiazib@sandia.gov>
2022-10-10 13:39:58 -0700

    EAGLES - save

---
+++ src/validation/calcsize/calcsize_driver.cpp
@@ -0,0 +1,61 @@
+#include <calcsize.hpp>
+
+#include <iostream>
+#include <skywalker.hpp>
+#include <validation.hpp>
+
+// This driver computes the binary or ternary nucleation rate for the given
+// input.
+
+void usage() {
+  std::cerr << "calsize_driver: a Skywalker driver for validating the "
+               "MAM4 calcsize parameterizations."
+            << std::endl;
+  std::cerr << "calcsize_driver: usage:" << std::endl;
+  std::cerr << "calcsize_driver <input.yaml>" << std::endl;
+  exit(0);
+}
+
+using namespace skywalker;
+using namespace mam4;
+
+// Parameterizations used by the nucleation process.
+void get_relaxed_v2n_limits(Ensemble* ensemble);
+
+int main(int argc, char** argv) {
+  if (argc == 1) {
+    usage();
+  }
+  Kokkos::initialize(argc, argv);
+  std::string input_file = argv[1];
+  std::string output_file = validation::output_name(input_file);
+  std::cout << argv[0] << ": reading " << input_file << std::endl;
+
+  // Load the ensemble. Any error encountered is fatal.
+  Ensemble* ensemble = skywalker::load_ensemble(input_file, "mam4xx");
+
+  // Figure out which component of nucleation we're validating by reading
+  // the settings.
+  Settings settings = ensemble->settings();
+  if (!settings.has("function")) {
+    std::cerr << "No function specified in mam4xx.settings!" << std::endl;
+    exit(0);
+  }
+
+  // Dispatch to the requested function.
+  auto func_name = settings.get("function");
+  try {
+    if (func_name == "get_relaxed_v2n_limits") {  // nucleation
+      get_relaxed_v2n_limits(ensemble);
+    } 
+  } catch (std::exception& e) {
+    std::cerr << argv[0] << ": Error: " << e.what() << std::endl;
+  }
+
+  // Write out a Python module.
+  std::cout << argv[0] << ": writing " << output_file << std::endl;
+  ensemble->write(output_file);
+
+  // Clean up.
+  delete ensemble;
+}
