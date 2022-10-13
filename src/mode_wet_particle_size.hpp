fa5492d43be3300700b451787c7033e5797a0987
Peter Bosler <pabosle@sandia.gov>
2022-10-13 09:20:55 -0600

    adding mode_wet_radius, a replacement for subroutine modal_aero_kohler

--- src/mode_wet_particle_size.hpp
+++ src/mode_wet_particle_size.hpp
@@ -12,6 +12,8 @@
 
 #include <ekat/ekat_pack.hpp>
 
+#include <sstream>
+
 namespace mam4 {
 
 static constexpr Real meters_to_microns = 1e6;
@@ -35,6 +37,20 @@ void mode_avg_wet_particle_diam(const Diagnostics& diags, const Prognostics& pro
   PackType rel_humidity = conversions::relative_humidity_from_vapor_mixing_ratio(
     atm.vapor_mixing_ratio(pack_idx), atm.pressure(pack_idx),
     atm.temperature(pack_idx));
+//   if ( (rel_humidity > 0.8).any() ) {
+    std::ostringstream ss;
+    ss << "T = " << atm.temperature(pack_idx) << " P = " << atm.pressure(pack_idx) << " qv = " << atm.vapor_mixing_ratio(pack_idx) << " rh = " << rel_humidity << "\n";
+    std::cout << ss.str();
+//   }
+  if (!FloatingPoint<PackType>::in_bounds(rel_humidity, 0, 1)) {
+    std::ostringstream ss;
+    ss << "error: relative humidity = " << rel_humidity << "\n";
+    ss << "\t" << "qv = " << atm.vapor_mixing_ratio(pack_idx) << "\n";
+    ss << "\t" << "p  = " << atm.pressure(pack_idx) << "\n";
+    ss << "\t" << "T  = " << atm.temperature(pack_idx) << "\n";
+    std::cout << ss.str();
+  }
+  EKAT_KERNEL_ASSERT(FloatingPoint<PackType>::in_bounds(rel_humidity, 0, 1));
 
   // set masks:
   //  case 1: dry air
@@ -51,35 +67,42 @@ void mode_avg_wet_particle_diam(const Diagnostics& diags, const Prognostics& pro
   const auto needs_kohler = !use_dry_radius;
   if (needs_kohler.any()) {
 
+    const PackType dry_radius_microns = 0.5*to_microns*diags.dry_geometric_mean_diameter[mode_idx](pack_idx);
+
     // Kohler polynomial solve requires double precision
-    typedef ekat::Pack<double, HAERO_PACK_SIZE> DoublePack;
+    typedef ekat::Pack<double, haero::HAERO_PACK_SIZE> DoublePack;
     typedef KohlerPolynomial<DoublePack> PolynomialType;
     typedef KohlerSolver<haero::math::NewtonSolver<PolynomialType>> SolverType;
     const Real tol = solver_convergence_tol;
 
-    const auto kpoly = PolynomialType(needs_kohler,
-      rel_humidity, diags.hygroscopicity(pack_idx),
-      0.5*to_microns*diags.dry_geometric_mean_diameter[mode_idx](pack_idx));
-
-    auto solver = SolverType(rel_humidity, diags.hygroscopicity(pack_idx),
-      0.5*to_microns*diags.dry_geometric_mean_diameter[mode_idx](pack_idx),
-      tol);
-    PackType rwet_microns = solver.solve();
+    auto kohler_solver = SolverType(rel_humidity, diags.hygroscopicity[mode_idx](pack_idx),
+      dry_radius_microns, tol);
+    PackType rwet_microns = kohler_solver.solve();
 
     wet_diam.set(needs_kohler, 2*to_meters*rwet_microns);
 
     // Apply hysteresis to intermediate relative humidities
     const PackType dry_vol = conversions::mean_particle_volume_from_diameter(
       diags.dry_geometric_mean_diameter[mode_idx](pack_idx),
-      modes[m].mean_std_dev);
+      modes[mode_idx].mean_std_dev);
 
     PackType wet_vol = conversions::mean_particle_volume_from_diameter(
-      2*to_meters*rwet_microns, modes[m].mean_std_dev);
+      2*to_meters*rwet_microns, modes[mode_idx].mean_std_dev);
 
     const Real hysteresis_factor =
       1 / (modes[mode_idx].deliquescence_pt - modes[mode_idx].crystallization_pt);
 
-    EKAT_KERNEL_ASSERT( (wet_vol >= dry_vol).all() );
+//     EKAT_KERNEL_ASSERT( (wet_vol >= dry_vol).all() );
+    if ( (wet_vol < dry_vol).any() ) {
+      std::ostringstream ss;
+      ss << "error: wet_vol = " << wet_vol << " dry_vol = " << dry_vol << "\n";
+      ss << "\t" << "hygroscopicity = " << diags.hygroscopicity[mode_idx](pack_idx) << "\n";
+      ss << "\t" << "dry_geometric_mean_diameter = " << diags.dry_geometric_mean_diameter[mode_idx](pack_idx) << "\n";
+      ss << "\t" << "dry_radius_microns = " << dry_radius_microns << "\n";
+      ss << "\t" << "relative humidity = " << rel_humidity << "\n";
+      ss << "\t" << "rwet_microns = " << rwet_microns << "\n";
+      std::cout << ss.str();
+    }
 
     const PackType water_vol = (wet_vol - dry_vol) *
       (rel_humidity - modes[mode_idx].crystallization_pt) * hysteresis_factor;
@@ -94,6 +117,14 @@ void mode_avg_wet_particle_diam(const Diagnostics& diags, const Prognostics& pro
   }
 
   diags.wet_geometric_mean_diameter[mode_idx](pack_idx) = wet_diam;
+}
+
+KOKKOS_INLINE_FUNCTION
+void mode_avg_wet_particle_diam(const Diagnostics& diags, const Prognostics& progs,
+  const Atmosphere& atm, const int pack_idx) {
+  for (int m=0; m<AeroConfig::num_modes(); ++m) {
+    mode_avg_wet_particle_diam(diags, progs, atm, m, pack_idx);
+  }
 }
 
 } // namespace mam4
