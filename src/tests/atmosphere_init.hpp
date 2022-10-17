ad3728e1ad1b06351e9db46d38476a308dcc20a9
Peter Bosler <pabosle@sandia.gov>
2022-10-17 11:43:17 -0600

    adds a unit test for relative humidity; other conversion functions remain untested. adds a haero::Atmosphere initializer to the testing library.

---
+++ src/tests/atmosphere_init.hpp
@@ -0,0 +1,104 @@
+#ifndef MAM4XX_ATMOSPHERE_INIT_HPP
+#define MAM4XX_ATMOSPHERE_INIT_HPP
+
+#include "mam4xx/mam4_types.hpp"
+
+#include <haero/haero.hpp>
+#include <haero/atmosphere.hpp>
+#include <haero/constants.hpp>
+#include <haero/floating_point.hpp>
+#include <ekat/ekat_pack.hpp>
+#include <ekat/ekat_pack_math.hpp>
+
+namespace mam4 {
+
+/** @brief Water vapor mixing ratio profile, @f$ q_v(z) @f$
+
+  @param [in] z height [m]
+  @param [in] qv0 mixing ratio at surface [kg H<sub>2</sub>O / kg dry air]
+  @param [in] qv1 decay rate of water vapor [1/m]
+  @return @f$ q_v @f$
+*/
+template <typename Scalar> KOKKOS_INLINE_FUNCTION
+Scalar init_specific_humidity(const Scalar z, const Real qv0, const Real qv1) {
+  Scalar result = qv0 * exp( - qv1 * z);
+
+  return result;
+}
+
+/** @brief Linear virtual temperature profile, @f$T_v(z)@f$.
+
+  @param [in] z height [m]
+  @param [in] T0 reference virtual temperature [K]
+  @param [in] Gamma virtual temperature lapse rate [K/m]
+  @return virtual temperature
+*/
+template <typename Scalar> KOKKOS_INLINE_FUNCTION
+Scalar init_virtual_temperature(const Scalar z, const Real Tv0, const Real Gammav) {
+  return Tv0 - Gammav * z;
+}
+
+/** @brief Computes the hydrostatic pressure at a given height, based on a
+  virtual temperature profile with constant lapse rate @f$ \Gamma =
+  -\frac{\partial T_v}{\partial z} \ge 0 @f$.
+
+  @param [in] z height [m]
+  @param [in] p0 reference presssure [Pa]
+  @param [in] T0 reference temperature [K]
+  @param [in] Gamma temperature lapse rate [K/m]
+  @return p [Pa]
+*/
+template <typename Scalar> KOKKOS_INLINE_FUNCTION
+Scalar hydrostatic_pressure_at_height(const Scalar z, const Real p0, const Real T0,
+                                    const Real Gamma) {
+  Scalar result = 0;
+  if (FloatingPoint<Real>::zero(Gamma)) {
+    result = p0 * exp(-Constants::gravity * z /
+                           (Constants::r_gas_dry_air * T0));
+  } else {
+    const Real pwr = Constants::gravity / (Constants::r_gas_dry_air * Gamma);
+    result = p0 * pow(T0, -pwr) * pow(T0 - Gamma * z, pwr);
+  }
+  return result;
+}
+
+/** @brief Computes the height based on hydrostatic pressure.
+
+    @param [in] p pressure [Pa]
+    @param [in] p0 reference pressure [Pa]
+    @param [in] T0 reference virtual temperature
+    @param [in] Gamma virtual temperature lapse rate, @f$\Gamma =
+   -\frac{\partial T_v}{\partial z} \ge 0 @f$
+    @return height [m]
+*/
+template <typename Scalar> KOKKOS_INLINE_FUNCTION
+Scalar height_at_hydrostatic_pressure(const Scalar p, const Real p0, const Real T0,
+                        const Real Gamma) {
+  Scalar result = 0;
+  if (FloatingPoint<Real>::zero(Gamma)) {
+    result =
+        -Constants::r_gas_dry_air * T0 * log(p / p0) / Constants::gravity;
+  } else {
+    const Real pwr = Constants::r_gas_dry_air * Gamma / Constants::gravity;
+    result = (T0 / Gamma) * (1 - pow(p / p0, pwr));
+  }
+  return result;
+}
+
+/** @brief Initializes a hydrostatic atmosphere column with linear
+  virtual temperature profile.
+
+  Levels are spaced uniformly in height, water vapor decays exponentially
+  with height, and virtual temperature decays linearly with height.
+
+  @param [in/out] atm Atmosphere column whose data need initialization
+  @param [in] Tv0 Reference virtual temperature (surface) [K]
+  @param [in] Gammav virtual temperature lapse rate [K/m]
+  @param [in] qv0 mixing ratio at surface [kg H<sub>2</sub>O / kg air]
+  @param [in] qv1 decay rate of water vapor [1/m]
+*/
+void init_atm_const_tv_lapse_rate(const Atmosphere& atm, const Real Tv0 = 300, const Real Gammav = 0.01,
+  const Real qv0=0.005, const Real qv1=1e-5);
+
+} // namespace mam4
+#endif
---
+++ src/tests/conversions_unit_tests.cpp
@@ -0,0 +1,119 @@
+#include "mam4xx/conversions.hpp"
+
+#include <catch2/catch.hpp>
+
+#include <haero/haero.hpp>
+#include <haero/atmosphere.hpp>
+#include <haero/constants.hpp>
+#include <haero/floating_point.hpp>
+
+#include "mam4xx/conversions.hpp"
+#include "atmosphere_init.hpp"
+
+#include <ekat/ekat_pack_math.hpp>
+#include <ekat/logging/ekat_logger.hpp>
+#include <ekat/mpi/ekat_comm.hpp>
+
+#include <cmath>
+#include <sstream>
+
+using namespace mam4;
+using namespace mam4::conversions;
+
+TEST_CASE("conversions", "") {
+  ekat::Comm comm;
+  ekat::logger::Logger<> logger("conversions unit tests", ekat::logger::LogLevel::debug, comm);
+
+  const int nlev = 72;
+  const int npacks = PackInfo::num_packs(nlev);
+  const Real pblh = 0;
+  Atmosphere atm(nlev, pblh);
+
+  // initialize a hydrostatically balanced moist air column
+  // using constant lapse rate in virtual temperature to manufacture
+  // exact solutions.
+  //
+  // these values correspond to a humid atmosphere with relative humidity
+  // values approximately between 30% and 80%
+  const Real Tv0 = 300; // reference virtual temperature [K]
+  const Real Gammav = 0.01; // virtual temperature lapse rate [K/m]
+  const Real qv0 = 0.015; // specific humidity at surface [kg h2o / kg moist air]
+  const Real qv1 = 7.5e-4; // specific humidity lapse rate [1 / m]
+  init_atm_const_tv_lapse_rate(atm, Tv0, Gammav, qv0, qv1);
+
+  const auto T = atm.temperature;
+  const auto P = atm.pressure;
+  const auto w = atm.vapor_mixing_ratio;
+
+  auto h_w = Kokkos::create_mirror_view(w);
+  auto h_T = Kokkos::create_mirror_view(T);
+  auto h_P = Kokkos::create_mirror_view(P);
+  auto h_z = Kokkos::create_mirror_view(atm.height);
+  auto h_hdp = Kokkos::create_mirror_view(atm.hydrostatic_dp);
+
+  SECTION("atm. init") {
+    Kokkos::deep_copy(h_w, w);
+    Kokkos::deep_copy(h_T, T);
+    Kokkos::deep_copy(h_P, P);
+    Kokkos::deep_copy(h_z, atm.height);
+    Kokkos::deep_copy(h_hdp, atm.hydrostatic_dp);
+
+    // write atm. column data to log for comparison with e3sm
+    for (int k=0; k<npacks; ++k) {
+      REQUIRE( (h_w(k) >= 0).all() );
+      logger.info("levek {}: T = {} P = {} z = {} dp = {} w = {}",
+        k, h_T(k), h_P(k), h_z(k), h_hdp(k), h_w(k));
+    }
+
+  }
+
+  SECTION("relative humidity") {
+    ColumnView specific_humidity("specific_humidity", npacks);
+    ColumnView relative_humidity_w("relative_humidity_w", npacks);
+    ColumnView relative_humidity_q("relative_humidity_q", npacks);
+
+    // compute relative humidity with respect to specific humidity and to
+    // mixing ratio
+    Kokkos::parallel_for(npacks, KOKKOS_LAMBDA (const int k) {
+      const auto q = specific_humidity_from_vapor_mixing_ratio(w(k));
+      specific_humidity(k) = q;
+      relative_humidity_q(k) =
+        relative_humidity_from_specific_humidity(q, T(k), P(k));
+      relative_humidity_w(k) =
+        relative_humidity_from_vapor_mixing_ratio(w(k), T(k), P(k));
+    });
+
+    auto h_q = Kokkos::create_mirror_view(specific_humidity);
+    auto h_rh_q = Kokkos::create_mirror_view(relative_humidity_q);
+    auto h_rh_w = Kokkos::create_mirror_view(relative_humidity_w);
+
+
+    Kokkos::deep_copy(h_q, specific_humidity);
+    Kokkos::deep_copy(h_rh_q, relative_humidity_q);
+    Kokkos::deep_copy(h_rh_w, relative_humidity_w);
+
+
+    for (int k=0; k<npacks; ++k) {
+      logger.info("level {}: T = {} P = {} w = {} q = {} relative humidity = {}",
+          k,  h_T(k), h_P(k), h_w(k), h_q(k), h_rh_q(k));
+      if (!haero::FloatingPoint<PackType>::in_bounds(h_rh_q(k), 0, 1)) {
+        logger.debug("\tlevel {}: w = {} wsat = {}",
+          k, vapor_mixing_ratio_from_specific_humidity(h_q(k)),
+            saturation_mixing_ratio_hardy(h_T(k), h_P(k)));
+      }
+      // check that relative humidities are in bounds
+      CHECK( haero::FloatingPoint<PackType>::in_bounds(h_rh_q(k), 0, 1) );
+      CHECK( haero::FloatingPoint<PackType>::in_bounds(h_rh_w(k), 0, 1.) );
+
+      // both relative humidities should match
+      if (!haero::FloatingPoint<PackType>::rel(h_rh_q(k), h_rh_w(k),
+        std::numeric_limits<float>::epsilon())) {
+        logger.debug("rel diff found at level {}: rh_q = {} rh_w = {} rel_diff = {}",
+          k, h_rh_q(k), h_rh_w(k), abs(h_rh_q(k) - h_rh_w(k))/h_rh_q(k));
+      }
+      REQUIRE(haero::FloatingPoint<PackType>::rel(h_rh_q(k), h_rh_w(k),
+        std::numeric_limits<float>::epsilon()));
+    }
+
+  }
+}
