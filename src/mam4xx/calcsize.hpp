23ff87794c2cafe23adebc8893e4b3618235d9b5
Oscar Diaz-Ibarra <odiazib@sandia.gov>
2022-11-16 09:38:30 -0700

    calcsize - fix typo

--- src/mam4xx/calcsize.hpp
+++ src/mam4xx/calcsize.hpp
@@ -559,7 +559,7 @@ public:
   // init -- initializes the implementation with MAM4's configuration and with
   // a process-specific configuration.
   void init(const AeroConfig &aero_config,
-            const Config &calsize_config = Config()) {
+            const Config &calcsize_config = Config()) {
     // Set nucleation-specific config parameters.
     config_ = calcsize_config;
 
