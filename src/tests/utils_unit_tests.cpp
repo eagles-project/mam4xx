a6956b2e1beb7d068a1393f40a608bb33613852a
Kyle Pressel <kyle.pressel@pnnl.gov>
2023-01-26 10:54:11 -0800

    Adding unit tests for utils.

---
+++ src/tests/utils_unit_tests.cpp
@@ -0,0 +1,26 @@
+#include <catch2/catch.hpp>
+#include <haero/math.hpp>
+#include <mam4xx/mam4.hpp>
+
+using Real = haero::Real;
+
+TEST_CASE("test_min_max_bound", "utils") {
+
+  Real low = 0.0;
+  Real high = 1.0;
+  Real num = 0.5;
+
+  /// if num is between low and high return num
+  Real ret = mam4::utils::min_max_bound(low, high, num);
+  REQUIRE(ret == num);
+
+  /// if num is greater than high return high
+  num = 1.5;
+  ret = mam4::utils::min_max_bound(low, high, num);
+  REQUIRE(ret == high);
+
+  // if num is less than low return low
+  num = -0.5;
+  ret = mam4::utils::min_max_bound(low, high, num);
+  REQUIRE(ret == low);
+}
