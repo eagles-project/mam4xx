cfa750faa4afdd64eb220e5a6de21fc5b8f65f8a
Jaelyn Litzinger <jaelyn.litzinger@pnnl.gov>
2023-01-27 11:28:32 -0800

    set PNNL CI gpu partition to dl_shared

--- .github/pnnl-ci/findIdleDLNodes.pl
+++ .github/pnnl-ci/findIdleDLNodes.pl
@@ -4,12 +4,17 @@ my @partitions = grep /^dl/, `sinfo`;
 # Idle nodes
 my @idle_parts = grep /idle/, @partitions;
 
-if ( scalar @idle_parts eq 0 ) {
+# Set to dl_shared for now
+# Change here for desired partition
+print "dl_shared";
 
-  # No idle dl parititons... Just choose the shared one.
-  print "dl_shared";
-}
-else {
-  my @parts = split / /, $idle_parts[0];
-  print $parts[0];
-}
+# Logic for selecting dl(v)(t) partitions with idle nodes
+#if ( scalar @idle_parts eq 0 ) {
+#
+#  # No idle dl parititons... Just choose the shared one.
+#  print "dl_shared";
+#}
+#else {
+#  my @parts = split / /, $idle_parts[0];
+#  print $parts[0];
+#}
