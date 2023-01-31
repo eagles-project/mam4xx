70fc4cea89ff1e5fea85fd2ba15c6f63d926701e
Kyle Pressel <kyle.pressel@pnnl.gov>
2023-01-31 10:58:47 -0800

    validation

---
+++ src/validation/coagulation/coag_1subarea.cpp
@@ -0,0 +1,10 @@
+#include <mam4xx/coagulation.hpp>
+#include <skywalker.hpp>
+#include <validation.hpp>
+
+using namespace skywalker;
+using namespace mam4;
+
+void coag_1subarea(Ensemble *ensemble) {
+
+}
