a77446968eec4cf38cf52f6bb8cd5241f53c334b
Jeffrey N. Johnson <jeff@cohere-llc.com>
2023-02-17 22:30:39 +0000

    Added LICENSE, AUTHORS, and license headers to source files.
    This lets us open up the mam4xx repo.

--- src/tests/atmosphere_utils.cpp
+++ src/tests/atmosphere_utils.cpp
@@ -1,3 +1,8 @@
+// mam4xx: Copyright (c) 2022,
+// Battelle Memorial Institute and
+// National Technology & Engineering Solutions of Sandia, LLC (NTESS)
+// SPDX-License-Identifier: BSD-3-Clause
+
 #include "atmosphere_utils.hpp"
 #include "mam4xx/conversions.hpp"
 
--- src/tests/mam4_calcsize_unit_tests.cpp
+++ src/tests/mam4_calcsize_unit_tests.cpp
@@ -1,3 +1,8 @@
+// mam4xx: Copyright (c) 2022,
+// Battelle Memorial Institute and
+// National Technology & Engineering Solutions of Sandia, LLC (NTESS)
+// SPDX-License-Identifier: BSD-3-Clause
+
 #include <mam4xx/mam4.hpp>
 
 #include <catch2/catch.hpp>
