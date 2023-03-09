88085c2273e77640385cfec82c8f0b5649761ce7
Oscar Diaz-Ibarra <odiazib@sandia.gov>
2023-03-09 15:54:24 -0700

    nucleate_ice - update validation test

--- src/validation/nucleate_ice/compute_tendencies.cpp
+++ src/validation/nucleate_ice/compute_tendencies.cpp
@@ -35,16 +35,17 @@ void compute_tendencies(Ensemble *ensemble) {
     mam4::AeroConfig mam4_config;
     NucleateIce::Config nucleate_ice_config(subgrid);
     mam4::NucleateIceProcess process(mam4_config, nucleate_ice_config);
-    const auto nmodes = mam4_config.num_modes();
-    const Real pmid = input.get("pressure");    // air pressure
-    const Real temp = input.get("temperature"); // air temperature
-    const Real updraft_vel_ice_nucleation =
-        input.get("updraft_vel_ice_nucleation"); // cloud fraction
-    const Real cloud_fraction = input.get("cloud_fraction");
-    const Real vapor_mixing_ratio = input.get("vapor_mixing_ratio");
 
-    auto q_i = input.get_array("interstitial");
-    auto n_i = input.get_array("interstitial_num");
+    const Real pmid = input.get_array("pmid")[0];    // air pressure
+    const Real temp = input.get_array("temperature")[0]; // air temperature
+    const Real updraft_vel_ice_nucleation =
+        input.get_array("wsubi")[0]; // cloud fraction
+    const Real cloud_fraction = input.get_array("ast")[0];
+
+    auto state_q = input.get_array("state_q");
+    // qn(pcols,pver)           ! water vapor mixing ratio [kg/kg]
+    // qn(:ncol,:pver) = state_q(:ncol,:pver,1)
+    const Real vapor_mixing_ratio = state_q[0];
 
     Kokkos::deep_copy(atm.temperature, temp);
     Kokkos::deep_copy(atm.pressure, pmid);
@@ -53,30 +54,60 @@ void compute_tendencies(Ensemble *ensemble) {
                       updraft_vel_ice_nucleation);
     Kokkos::deep_copy(atm.vapor_mixing_ratio, vapor_mixing_ratio);
 
-    const Real dry_diameter_aitken = input.get("dry_diameter_aitken");
+    auto numptr_amode = input.get_array("numptr_amode");
+    auto modeptr_aitken = input.get_array("modeptr_aitken")[0]-1;
+    auto modeptr_coarse = input.get_array("modeptr_coarse")[0]-1;
+
+    // mode number mixing ratios
+    auto num_aitken = state_q[numptr_amode[modeptr_aitken]-1];
+    auto num_coarse = state_q[numptr_amode[modeptr_coarse]-1];
+
+
+    // we only copy values of mass m.r that are use in this process. 
+    // Other values will be equal to zero
     const int aitken_idx = int(ModeIndex::Aitken);
+    const int coarse_idx = int(ModeIndex::Coarse);
+    Kokkos::deep_copy(progs.n_mode_i[aitken_idx], num_aitken);
+    Kokkos::deep_copy(progs.n_mode_i[coarse_idx], num_coarse);
+
+    auto lptr_dust_a_amode = input.get_array("lptr_dust_a_amode");
+    auto lptr_nacl_a_amode = input.get_array("lptr_nacl_a_amode");
+    auto lptr_so4_a_amode = input.get_array("lptr_so4_a_amode");
+    auto lptr_mom_a_amode = input.get_array("lptr_mom_a_amode");
+    auto lptr_bc_a_amode = input.get_array("lptr_bc_a_amode");
+    auto lptr_pom_a_amode = input.get_array("lptr_pom_a_amode");
+    auto lptr_soa_a_amode = input.get_array("lptr_soa_a_amode");
+
+    // mode specie mass m.r. 
+    // -1 because of indexing in C++
+    auto coarse_dust = state_q[lptr_dust_a_amode[modeptr_coarse]-1];
+    auto coarse_nacl = state_q[lptr_nacl_a_amode[modeptr_coarse]-1];
+    auto coarse_so4  = state_q[lptr_so4_a_amode[modeptr_coarse]-1];
+    auto coarse_mom  = state_q[lptr_mom_a_amode[modeptr_coarse]-1];
+    auto coarse_bc   = state_q[lptr_bc_a_amode[modeptr_coarse]-1];
+    auto coarse_pom  = state_q[lptr_pom_a_amode[modeptr_coarse]-1];
+    auto coarse_soa  = state_q[lptr_soa_a_amode[modeptr_coarse]-1];
+
+    const int dst_idx = int(AeroId::DST);
+    const int nacl_idx = int(AeroId::NaCl);
+    const int so4_idx = int(AeroId::SO4);
+    const int mom_idx = int(AeroId::MOM);
+    const int bc_idx = int(AeroId::BC);
+    const int pom_idx = int(AeroId::POM);
+    const int soa_idx = int(AeroId::SOA);
+    // we only copy values of mass m.r that are use in this process. 
+    // Other values will be equal to zero
+    Kokkos::deep_copy(progs.q_aero_i[coarse_idx][dst_idx], coarse_dust);
+    Kokkos::deep_copy(progs.q_aero_i[coarse_idx][nacl_idx],coarse_nacl);
+    Kokkos::deep_copy(progs.q_aero_i[coarse_idx][so4_idx], coarse_so4);
+    Kokkos::deep_copy(progs.q_aero_i[coarse_idx][mom_idx], coarse_mom);
+    Kokkos::deep_copy(progs.q_aero_i[coarse_idx][bc_idx], coarse_bc);
+    Kokkos::deep_copy(progs.q_aero_i[coarse_idx][pom_idx], coarse_pom);
+    Kokkos::deep_copy(progs.q_aero_i[coarse_idx][soa_idx], coarse_soa);
+
+    auto dgnum = input.get_array("dgnum");
     Kokkos::deep_copy(diags.dry_geometric_mean_diameter_i[aitken_idx],
-                      dry_diameter_aitken);
-
-    int count = 0;
-    for (int imode = 0; imode < nmodes; ++imode) {
-
-      auto h_prog_n_mode_i = Kokkos::create_mirror_view(progs.n_mode_i[imode]);
-      h_prog_n_mode_i(0) = n_i[imode];
-      Kokkos::deep_copy(progs.n_mode_i[imode], h_prog_n_mode_i);
-
-      const auto n_spec = num_species_mode(imode);
-      for (int isp = 0; isp < n_spec; ++isp) {
-        // correcting index for inputs.
-        const int isp_mam4xx =
-            validation::e3sm_to_mam4xx_aerosol_idx[imode][isp];
-        auto h_prog_aero_i =
-            Kokkos::create_mirror_view(progs.q_aero_i[imode][isp_mam4xx]);
-        h_prog_aero_i(0) = q_i[count];
-        Kokkos::deep_copy(progs.q_aero_i[imode][isp_mam4xx], h_prog_aero_i);
-        count++;
-      } // end species
-    }   // end modes
+                      dgnum[modeptr_aitken]);
 
     auto team_policy = ThreadTeamPolicy(1u, Kokkos::AUTO);
     Kokkos::parallel_for(
@@ -100,9 +131,9 @@ void compute_tendencies(Ensemble *ensemble) {
         Kokkos::create_mirror_view(diags.icenuc_num_meydep);
     Kokkos::deep_copy(h_icenuc_num_meydep, diags.icenuc_num_meydep);
 
-    output.set("icenuc_num_hetfrz", h_icenuc_num_hetfrz[0]);
-    output.set("icenuc_num_immfrz", h_icenuc_num_immfrz[0]);
-    output.set("icenuc_num_depnuc", h_icenuc_num_depnuc[0]);
-    output.set("icenuc_num_meydep", h_icenuc_num_meydep[0]);
+    // output.set("icenuc_num_hetfrz", h_icenuc_num_hetfrz[0]);
+    // output.set("icenuc_num_immfrz", h_icenuc_num_immfrz[0]);
+    // output.set("icenuc_num_depnuc", h_icenuc_num_depnuc[0]);
+    // output.set("icenuc_num_meydep", h_icenuc_num_meydep[0]);
   });
 }
