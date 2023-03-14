5c467652f5318c14572cc222ae51c595b20ab901
Oscar Diaz-Ibarra <odiazib@sandia.gov>
2023-03-14 13:34:26 -0600

    nucleate_ice - one more validation test from mam4_standalone_validation

--- src/validation/nucleate_ice/nucleate_ice.cpp
+++ src/validation/nucleate_ice/nucleate_ice.cpp
@@ -25,17 +25,29 @@ void nucleate_ice_test(Ensemble *ensemble) {
     const Real zero = 0;
     Real nuci, onihf, oniimm, onidep, onimey = zero;
 
-    const Real pmid = input.get_array("pmid")[0]; // air pressure
-    const Real temp = input.get_array("tair")[0]; // air temperature
-    const Real cloud_fraction = input.get_array("cldn")[0];
-    // updraft_vel_ice_nucleation
-    const Real wbar = input.get_array("wbar")[0];
-    const Real relhum = input.get_array("relhum")[0];
-    const Real rhoair = input.get_array("rhoair")[0];
-    const Real so4_num = input.get_array("so4_num")[0];
-    const Real dst3_num = input.get_array("dst3_num")[0];
+    // get values from input when using either a 1D vector or a scalar
+    auto get_value = [=](const std::string var_name) {
+      if (input.has_array(var_name)) {
+        return input.get_array(var_name)[0]; //
+      } else if (input.has(var_name)) {
+        return input.get(var_name);
+      } else {
+        std::cerr << "Required name: " << var_name << std::endl;
+        exit(1);
+      }
+    };
 
-    const Real subgrid = input.get_array("subgrid")[0];
+    const Real pmid = get_value("pmid"); // air pressure
+    const Real temp = get_value("tair"); // air temperature
+    const Real cloud_fraction = get_value("cldn");
+    // updraft_vel_ice_nucleation
+    const Real wbar = get_value("wbar");
+    const Real relhum = get_value("relhum");
+    const Real rhoair = get_value("rhoair");
+    const Real so4_num = get_value("so4_num");
+    const Real dst3_num = get_value("dst3_num");
+
+    const Real subgrid = get_value("subgrid");
 
     this_nucleate_ice.nucleati(wbar, temp, pmid, relhum, cloud_fraction, rhoair,
                                so4_num, dst3_num, subgrid,
