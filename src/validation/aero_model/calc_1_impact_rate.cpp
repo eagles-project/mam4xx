63f16a56cbd84d3623cc1a941ee69ad39fa7ff7d
Oscar Diaz-Ibarra <odiazib@sandia.gov>
2023-04-05 14:41:58 -0600

    aero_model - missing file

---
+++ src/validation/aero_model/calc_1_impact_rate.cpp
@@ -0,0 +1,41 @@
+// mam4xx: Copyright (c) 2022,
+// Battelle Memorial Institute and
+// National Technology & Engineering Solutions of Sandia, LLC (NTESS)
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include <mam4xx/mam4.hpp>
+
+#include <mam4xx/aero_config.hpp>
+#include <skywalker.hpp>
+#include <validation.hpp>
+
+using namespace skywalker;
+using namespace mam4;
+
+void calc_1_impact_rate(Ensemble *ensemble) {
+  ensemble->process([=](const Input &input, Output &output) {
+    const Real zero = 0;
+    Real scavratenum = zero;
+    Real scavratevol = zero;
+    int lunerr = 0;
+    const Real dg0 = input.get_array("dg0")[0];
+    const Real sigmag = input.get_array("sigmag")[0];
+    const Real rhoaero = input.get_array("rhoaero")[0];
+    const Real temp = input.get_array("temp")[0];
+    const Real press = input.get_array("press")[0];
+    lunerr = int(input.get_array("lunerr")[0]);
+    // const Real input.get_array("")[0]
+
+    aero_model_od::calc_1_impact_rate(dg0,         //  in
+                                      sigmag,      //  in
+                                      rhoaero,     //  in
+                                      temp,        //  in
+                                      press,       //  in
+                                      scavratenum, // out
+                                      scavratevol, // out
+                                      lunerr);
+
+    output.set("scavratenum", std::vector(1, scavratenum));
+    output.set("scavratevol", std::vector(1, scavratevol));
+  });
+}
