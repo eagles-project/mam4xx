9c9f2374532d30336749549579cb350877949171
Cameron Rutherford <cameron.rutherford@me.com>
2023-05-23 13:26:39 -0400

    Use gcc 10 for deception CI

--- .github/pnnl-ci/ci.sh
+++ .github/pnnl-ci/ci.sh
@@ -41,8 +41,8 @@ echo $PRECISION "detected for PRECISION"
 
 . /etc/profile.d/modules.sh
 module purge
-module load cmake
-module load gcc/9.1.0
+module load cmake/3.21.4
+module load gcc/10.2.0
 module load cuda/11.4
 module load python/3.7.0
 
