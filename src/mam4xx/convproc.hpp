8dde31b3ab8b577e1bee20ebe5e2e58af61480c7
James Overfelt <jroverf@sandia.gov>
2023-05-23 08:56:48 -0600

    Clang format.

--- src/mam4xx/convproc.hpp
+++ src/mam4xx/convproc.hpp
@@ -1,4 +1,4 @@
-// mam4xx: Copyright (c) 2022, 
+// mam4xx: Copyright (c) 2022,
 // Battelle Memorial Institute and
 // National Technology & Engineering Solutions of Sandia, LLC (NTESS)
 // SPDX-License-Identifier: BSD-3-Clause
