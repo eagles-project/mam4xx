fdec6b5e84bdcec9a6335a7b6c96c391edd55f16
Jaelyn Litzinger <jaelyn.litzinger@pnnl.gov>
2023-06-01 15:31:49 -0700

    outline update_from_explmix

--- src/mam4xx/ndrop.hpp
+++ src/mam4xx/ndrop.hpp
@@ -152,6 +152,247 @@ void maxsat(
   return;
 } // end maxsat
 
+#if 0
+KOKKOS_INLINE_FUNCTION
+void update_from_explmix(const Real dtmicro,  // time step for microphysics [s]
+                         const Real csbot_k,    // air density at bottom (interface) of layer [kg/m^3]
+                         const Real csbot_km1,    // air density at bottom (interface) of layer [kg/m^3]
+                         const Real cldn_k,     // cloud fraction [fraction]
+                         const Real cldn_km1,     // cloud fraction [fraction]
+                         const Real cldn_kp1,     // cloud fraction [fraction]
+                         const Real zn_k,       // g/pdel for layer [m^2/kg]
+                         const Real zs_k,       // inverse of distance between levels [m^-1]
+                         const Real zs_km1,       // inverse of distance between levels [m^-1]
+                         const Real ekd_k,      //diffusivity for droplets [m^2/s]
+                         const Real ekd_km1,      //diffusivity for droplets [m^2/s]
+                         Real nact[AeroConfig::num_modes()], // fractional aero. number  activation rate [/s]
+                         Real mact[AeroConfig::num_modes()], // fractional aero. mass    activation rate [/s]
+                         Real qcld, // cloud droplet number mixing ratio [#/kg]
+                         Real raercol_k[ncnst_tot][2], // single column of saved aerosol mass, number mixing ratios [#/kg or kg/kg]
+                         Real raercol_cw_k[ncnst_tot][2], // same as raercol but for cloud-borne phase [#/kg or kg/kg]
+                         int nsav, // indices for old, new time levels in substepping
+                         int nnew  // indices for old, new time levels in substepping
+                         ) {
+  
+
+    // local arguments
+    int kk;           // vertical level index
+    int kp1;         // kk+1
+    int km1;          // kk-1
+    int imode;       // mode counter variable
+    int mm;          // local array index for MAM number, species
+    int lspec;       // species counter variable
+    int nsubmix, nsubmix_bnd;  // number of substeps and bound
+    int ntemp;   // temporary index for substepping
+    int isub;       // substep index
+
+    Real overlap_cld_thresh = 1e-10;  //  threshold cloud fraction to compute overlap [fraction]
+
+    Real ekk_k;       // density*diffusivity for droplets [kg/m/s]
+    Real ekk_km1;       // density*diffusivity for droplets [kg/m/s]
+    Real dtmin;     // time step to determine subloop time step [s]
+    Real qncld;     // updated cloud droplet number mixing ratio [#/kg]
+    Real ekkp;      // zn*zs*density*diffusivity [/s]
+    Real ekkm;      // zn*zs*density*diffusivity   [/s]
+    Real overlapp;  // cloud overlap involving level kk+1 [fraction]
+    Real overlapm;  // cloud overlap involving level kk-1 [fraction]
+    Real source_k;    //  source rate for activated number or species mass [/s]
+    Real tinv;      // inverse timescale of droplet diffusivity [/s]
+    Real dtt;       // timescale of droplet diffusivity [s]
+    Real dtmix;     // timescale for subloop [s]
+    Real tmpa;      //  temporary aerosol tendency variable [/s]
+    Real srcn_k;      // droplet source rate [/s]
+
+    // load new droplets in layers above, below clouds
+
+    dtmin = dtmicro;
+    //ekk= 0.0;
+  
+       // rce-comment -- ekd(k) is eddy-diffusivity at k/k+1 interface
+       //   want ekk(k) = ekd(k) * (density at k/k+1 interface)
+       //   so use pint(i,k+1) as pint is 1:pverp
+       //           ekk(k)=ekd(k)*2.*pint(i,k)/(rair*(temp(i,k)+temp(i,k+1)))
+       //           ekk(k)=ekd(k)*2.*pint(i,k+1)/(rair*(temp(i,k)+temp(i,k+1)))
+    ekk_k = ekd_k * csbot_k;
+    ekk_km1 = ekd_km1 * csbot_km1;
+
+    kp1 = min(kk+1, pver);
+    km1 = max(kk-1, top_lev);
+
+    // maximum overlap assumption
+    if (cldn_kp1 > overlap_cld_thresh) {
+      overlapp = haero::min(cldn_k/cldn_kp1, 1);
+    } else {
+      overlapp = 1;
+    }
+    
+    if (cldn_km1 > overlap_cld_thresh) {
+      overlapm = haero::min(cldn_k/cldn_km1, 1);
+    } else {
+      overlapm = 1;
+    }
+
+    ekkp = zn_k * ekk * zs_k;
+    ekkm = zn_k * ekk_km1 * zs_km1;
+    tinv = ekkp + ekkm;
+
+       // rce-comment -- tinv is the sum of all first-order-loss-rates
+       //    for the layer.  for most layers, the activation loss rate
+       //    (for interstitial particles) is accounted for by the loss by
+       //    turb-transfer to the layer above.
+       //    k=pver is special, and the loss rate for activation within
+       //    the layer must be added to tinv.  if not, the time step
+       //    can be too big, and explmix can produce negative values.
+       //    the negative values are reset to zero, resulting in an
+       //    artificial source.
+
+    if (tinv > 1.e-6_r8) {
+      dtt   = 1.0 / tinv;
+      dtmin = haero::min(dtmin, dtt);
+    }
+    
+
+    dtmix = 0.9 * dtmin;
+    nsubmix = dtmicro / dtmix + 1;
+    if (nsubmix > 100) {
+       nsubmix_bnd = 100;
+    } else {
+       nsubmix_bnd = nsubmix;
+    }
+    dtmix = dtmicro / nsubmix;
+
+    // rce-comment
+    //    the activation source(k) = mact(k,m)*raercol(kp1,lmass)
+    //       should not exceed the rate of transfer of unactivated particles
+    //       from kp1 to k which = ekkp(k)*raercol(kp1,lmass)
+    //    however it might if things are not "just right" in subr activate
+    //    the following is a safety measure to avoid negatives in explmix
+
+    for(int imode = 0; imode < ntot_amode; imode++) {
+      nact[imode] = haero::min( nact[imode], ekkp);
+      mact[imode] = haero::min( mact[imode], ekkp);
+    }
+
+
+    // old_cloud_nsubmix_loop
+
+    //  Note:  each pass in submix loop stores updated aerosol values at index nnew,
+    //  current values at index nsav.  At the start of each pass, nnew values are
+    //  copied to nsav.  However, this is accomplished by switching the values
+    //  of nsav and nnew rather than a physical copying.  At end of loop nnew stores
+    //  index of most recent updated values (either 1 or 2).
+
+    for(int isub = 0; isub < nsubmix; isub++) {
+       qncld = qcld;
+       // after first pass, switch nsav, nnew so that nsav is the recently updated aerosol
+       if(isub > 0) {
+          ntemp = nsav;
+          nsav = nnew;
+          nnew = ntemp;
+       }
+       srcn = 0.0;
+
+       for(int imode = 0; imode < ntot_amode; imode++) {
+          mm = mam_idx(imode,0); //does this 0 need to change?
+
+          // update droplet source
+
+          // rce-comment- activation source in layer k involves particles from k+1
+          //	       srcn(:)=srcn(:)+nact(:,m)*(raercol(:,mm,nsav))
+          srcn(top_lev:pver-1) = srcn(top_lev:pver-1) + nact(top_lev:pver-1,imode)*(raercol(top_lev+1:pver,mm,nsav))
+
+          // rce-comment- new formulation for k=pver
+          //              srcn(  pver  )=srcn(  pver  )+nact(  pver  ,m)*(raercol(  pver,mm,nsav))
+          tmpa = raercol(pver,mm,nsav)*nact(pver,imode) &
+               + raercol_cw(pver,mm,nsav)*nact(pver,imode)
+          srcn(pver) = srcn(pver) + max(0.0_r8,tmpa)
+       } 
+       explmix(  qcld, &   // out
+            srcn, ekkp, ekkm, overlapp,  &   // in
+            overlapm, qncld,  &  // in
+            dtmix);     // in
+       // update aerosol number
+
+       // rce-comment
+       //    the interstitial particle mixratio is different in clear/cloudy portions
+       //    of a layer, and generally higher in the clear portion.  (we have/had
+       //    a method for diagnosing the the clear/cloudy mixratios.)  the activation
+       //    source terms involve clear air (from below) moving into cloudy air (above).
+       //    in theory, the clear-portion mixratio should be used when calculating
+       //    source terms
+       for(int imode = 0; imode < ntot_amode; imode++) {
+          mm = mam_idx(imode,0);
+          // rce-comment -   activation source in layer k involves particles from k+1
+          //	              source(:)= nact(:,m)*(raercol(:,mm,nsav))
+          source(top_lev:pver-1) = nact(top_lev:pver-1,imode)*(raercol(top_lev+1:pver,mm,nsav))
+          // rce-comment - new formulation for k=pver
+          //               source(  pver  )= nact(  pver,  m)*(raercol(  pver,mm,nsav))
+          tmpa = raercol(pver,mm,nsav)*nact(pver,imode) &
+               + raercol_cw(pver,mm,nsav)*nact(pver,imode)
+          source(pver) = max(0.0_r8, tmpa)
+
+          call explmix( raercol_cw(:,mm,nnew), &  // out
+               source, ekkp, ekkm, overlapp, &   // in
+               overlapm, raercol_cw(:,mm,nsav),   &  // in
+               dtmix, .false. )  // in
+          call explmix( raercol(:,mm,nnew), &   // out
+               source, ekkp, ekkm, overlapp,  &    // in
+               overlapm, raercol(:,mm,nsav),  &  // in
+               dtmix, .true., &  // in
+               raercol_cw(:,mm,nsav))  // optional in
+
+          // update aerosol species mass
+
+          for(int lspec = 0; lspec < nspec_amode[imode]; lspec++) {
+             mm = mam_idx(imode,lspec);
+             // rce-comment -   activation source in layer k involves particles from k+1
+             //	          source(:)= mact(:,m)*(raercol(:,mm,nsav))
+             source(top_lev:pver-1) = mact(top_lev:pver-1,imode)*(raercol(top_lev+1:pver,mm,nsav))
+             // rce-comment- new formulation for k=pver
+             //                 source(  pver  )= mact(  pver  ,m)*(raercol(  pver,mm,nsav))
+             tmpa = raercol(pver,mm,nsav)*mact(pver,imode) &
+                  + raercol_cw(pver,mm,nsav)*mact(pver,imode)
+             source(pver) = max(0.0_r8, tmpa)
+
+             explmix( raercol_cw(:,mm,nnew), &  // out
+                  source, ekkp, ekkm, overlapp, &  // in
+                  overlapm, raercol_cw(:,mm,nsav),    &  // in
+                  dtmix); // in
+             explmix( raercol(:,mm,nnew), &  // out
+                  source, ekkp, ekkm, overlapp,  &  // in
+                  overlapm, raercol(:,mm,nsav),  &   // in
+                  dtmix, .true., & // in
+                  raercol_cw(:,mm,nsav))  // optional in
+
+          }  // lspec loop
+       }  //  imode loop
+
+    } // old_cloud_nsubmix_loop
+
+    // evaporate particles again if no cloud
+
+    if (cldn_k == 0) {
+      // no cloud
+      qcld = 0.0;
+
+      // convert activated aerosol to interstitial in decaying cloud
+      for(int imode = 0; imode < ntot_amode; imode++) {
+        mm = mam_idx(imode,0);
+        raercol_k[mm][nnew] += raercol_cw_k[mm][nnew];
+        raercol_cw_k[mm][nnew] = 0.0;
+
+        for(int lspec = 0; lspec < nspec_amode[imode]; lspec++) {
+          mm = mam_idx(imode,lspec);
+          raercol_k[mm][nnew] += raercol_cw_k[mm][nnew];
+          raercol_cw_k[mm][nnew] = 0.0;
+        } 
+      }
+    }
+    
+
+} // end update_from_explmix
+#endif
+
 } // namespace ndrop
 } // namespace mam4
 #endif
