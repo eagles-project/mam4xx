2e844ae6a88c69556692bf85d0203750faafb896
Kyle Pressel <13586381+pressel@users.noreply.github.com>
2023-06-06 16:34:28 -0700

    Taking out extraneous preprocessor directive.

--- src/tests/mam4_test_config.hpp
+++ src/tests/mam4_test_config.hpp
@@ -1,10 +1,6 @@
 #ifndef MAM4_TEST_CONFIG_HPP
 #define MAM4_TEST_CONFIG_HPP
 
-// clang-format off
-#define MAM4_TEST_DATA_DIR "/Users/pres026/Research/EAGLES/mam4xx/src/tests/data"
-// clang-format on
-
 namespace mam4 {}
 
 #endif
