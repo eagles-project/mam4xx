277fb4f7812f27fa614fdb4a096a8815df52d9f3
Jeffrey N. Johnson <jeff@cohere-llc.com>
2023-06-08 12:00:16 -0700

    Modifying a hetfrz validation routine for new Atmosphere fields.

--- src/validation/hetfrz/hetfrz_rates_1box.cpp
+++ src/validation/hetfrz/hetfrz_rates_1box.cpp
@@ -384,7 +384,6 @@ void hetfrz_rates_1box(Ensemble *ensemble) {
 
     const int nlev = qc.size();
     Real pblh = 1000;
-    Atmosphere atm = validation::create_atmosphere(nlev, pblh);
     mam4::Prognostics progs = validation::create_prognostics(nlev);
     mam4::Diagnostics diags = validation::create_diagnostics(nlev);
     mam4::Tendencies tends = validation::create_tendencies(nlev);
@@ -633,22 +632,35 @@ void hetfrz_rates_1box(Ensemble *ensemble) {
     for (int k = 0; k < nlev; ++k) {
       host_column(k) = temperature[k];
     }
-    Kokkos::deep_copy(atm.temperature, host_column);
+    auto d_temperature = validation::create_column_view(nlev);
+    Kokkos::deep_copy(d_temperature, host_column);
 
     for (int k = 0; k < nlev; ++k) {
       host_column(k) = pmid[k];
     }
-    Kokkos::deep_copy(atm.pressure, host_column);
+    auto d_pressure = validation::create_column_view(nlev);
+    Kokkos::deep_copy(d_pressure, host_column);
 
     for (int k = 0; k < nlev; ++k) {
       host_column(k) = qc[k];
     }
-    Kokkos::deep_copy(atm.liquid_mixing_ratio, host_column);
+    auto d_liquid_mixing_ratio = validation::create_column_view(nlev);
+    Kokkos::deep_copy(d_liquid_mixing_ratio, host_column);
 
     for (int k = 0; k < nlev; ++k) {
       host_column(k) = nc[k];
     }
-    Kokkos::deep_copy(atm.cloud_liquid_number_mixing_ratio, host_column);
+    auto d_cloud_liquid_number_mixing_ratio = validation::create_column_view(nlev);
+    Kokkos::deep_copy(d_cloud_liquid_number_mixing_ratio, host_column);
+
+    // Because the Atmosphere type uses ConstColumnViews, we have to construct
+    // an atm object with views for all its data or set them piecemeal.
+    Atmosphere atm;
+    atm.temperature = d_temperature;
+    atm.pressure = d_pressure;
+    atm.liquid_mixing_ratio = d_liquid_mixing_ratio;
+    atm.cloud_liquid_number_mixing_ratio = d_cloud_liquid_number_mixing_ratio;
+    atm.planetary_boundary_layer_height = pblh;
 
     for (int k = 0; k < nlev; ++k) {
       host_column(k) = ast[k];
