2a913ec2a43921c21ed986245e21e40e2e2241d9
Kyle Pressel <13586381+pressel@users.noreply.github.com>
2023-07-11 16:16:36 -0700

    Adding the basics of dry dep.

---
+++ src/mam4xx/drydep.hpp
@@ -0,0 +1,58 @@
+// mam4xx: Copyright (c) 2022,
+// Battelle Memorial Institute and
+// National Technology & Engineering Solutions of Sandia, LLC (NTESS)
+// SPDX-License-Identifier: BSD-3-Clause
+
+#ifndef MAM4XX_DRYDEP_HPP
+#define MAM4XX_DRYDEP_HPP
+
+#include <haero/atmosphere.hpp>
+#include <mam4xx/aero_config.hpp>
+
+namespace mam4 {
+
+class DryDep {
+
+public:
+  struct Config {
+
+    Config(){};
+
+    Config(const Config &) = default;
+    ~Config() = default;
+    Config &operator=(const Config &) = default;
+  };
+
+private:
+  Config config_;
+
+public:
+  // name -- unique name of the process implemented by this class
+  const char *name() const { return "MAM4 dry deposition"; }
+
+  // init -- initializes the implementation with MAM4's configuration
+  void init(const AeroConfig &aero_config,
+            const Config &process_config = Config());
+
+  // validate -- validates the given atmospheric state and prognostics against
+  // assumptions made by this implementation, returning true if the states are
+  // valid, false if not
+  KOKKOS_INLINE_FUNCTION
+  bool validate(const AeroConfig &config, const ThreadTeam &team,
+                const Atmosphere &atm, const Prognostics &progs) const;
+
+  // compute_tendencies -- computes tendencies and updates diagnostics
+  // NOTE: that both diags and tends are const below--this means their views
+  // NOTE: are fixed, but the data in those views is allowed to vary.
+  KOKKOS_INLINE_FUNCTION
+  void compute_tendencies(const AeroConfig &config, const ThreadTeam &team,
+                          Real t, Real dt, const Atmosphere &atm,
+                          const Prognostics &progs, const Diagnostics &diags,
+                          const Tendencies &tends) const;
+};
+
+namespace hetfrz {}
+
+} // namespace mam4
+
+#endif
