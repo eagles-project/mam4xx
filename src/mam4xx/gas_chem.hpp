42e4822b5b6338d0977c3d330028ecf8fdf58dff
Michael J Schmidt <mjschm@sandia.gov>
2023-07-19 12:29:55 -0600

    cosmetic changes to gas_chem

--- src/mam4xx/gas_chem.hpp
+++ src/mam4xx/gas_chem.hpp
@@ -18,58 +18,54 @@ namespace mam4 {
 
 namespace gas_chemistry {
 
+// BAD CONSTANTs
 const int itermax = 11;
-const Real rel_err = 1.e-3;
-const Real high_rel_err = 1.e-4;
+const Real rel_err = 1.0e-3;
+const Real high_rel_err = 1.0e-4;
 const int max_time_steps = 1000;
+
+// initialize the solver (error tolerance)
 KOKKOS_INLINE_FUNCTION
 void imp_slv_inti(Real epsilon[clscnt4]) {
-  // const Real zero = 0;
-  // Real eps[clscnt4] = {zero};
-  // for (int i = 0; i < clscnt4; ++i) {
-  //   eps[i] = rel_err;
-  // }
-
-  // int ox_ndx = int(SpeciesId::O3);
-  // eps[ox_ndx] = high_rel_err;
-  // FIXME: follow fortran code.
   for (int i = 0; i < clscnt4; ++i) {
-    epsilon[i] = rel_err; // eps[clsmap_4[i]];
+    epsilon[i] = rel_err;
   }
 }
 
 KOKKOS_INLINE_FUNCTION
-void newton_raphson_iter(
-    const Real dti, const Real lin_jac[nzcnt], const Real lrxt[rxntot],
-    const Real lhet[gas_pcnst],         // & ! in
-    const Real iter_invariant[clscnt4], //              & ! in
-    const bool factor[itermax], const int permute_4[gas_pcnst],
-    const int clsmap_4[gas_pcnst], Real lsol[gas_pcnst],
-    Real solution[clscnt4],                     //              & ! inout
-    bool converged[clscnt4], bool &convergence, //         & ! out
-    Real prod[clscnt4], Real loss[clscnt4], Real max_delta[clscnt4],
-    // work arrays
-    Real epsilon[clscnt4]) {
+void newton_raphson_iter(const Real dti, const Real lin_jac[nzcnt],
+                         const Real lrxt[rxntot],
+                         const Real lhet[gas_pcnst],         // in
+                         const Real iter_invariant[clscnt4], // in
+                         const bool factor[itermax],
+                         const int permute_4[gas_pcnst],
+                         const int clsmap_4[gas_pcnst], Real lsol[gas_pcnst],
+                         Real solution[clscnt4],                     // inout
+                         bool converged[clscnt4], bool &convergence, // out
+                         Real prod[clscnt4], Real loss[clscnt4],
+                         Real max_delta[clscnt4],
+                         // work array
+                         Real epsilon[clscnt4]) {
 
-  // !-----------------------------------------------------
-  // ! the newton-raphson iteration for f(y) = 0
-  // !-----------------------------------------------------
+  // -----------------------------------------------------
+  //  the newton-raphson iteration for f(y) = 0
+  // -----------------------------------------------------
 
   Real sys_jac[nzcnt] = {};
   Real forcing[clscnt4] = {};
   // BAD CONSTANT
-  const Real small = 1.e-40;
+  const Real small = 1.0e-40;
   const Real zero = 0;
 
   for (int nr_iter = 0; nr_iter < itermax; ++nr_iter) {
-    // !-----------------------------------------------------------------------
-    // ! ... the non-linear component
-    // !-----------------------------------------------------------------------
+    // -----------------------------------------------------------------------
+    //  ... the non-linear component
+    // -----------------------------------------------------------------------
 
     if (factor[nr_iter]) {
-      nlnmat(sys_jac, //   & ! out
+      nlnmat(sys_jac, // out
              lin_jac,
-             dti); // ! in
+             dti); // in
       // -----------------------------------------------------------------------
       //  ... factor the "system" matrix
       // -----------------------------------------------------------------------
@@ -77,35 +73,34 @@ void newton_raphson_iter(
       lu_fac(sys_jac);
 
     } // factor
-    // !-----------------------------------------------------------------------
-    // ! ... form f(y)
-    // !-----------------------------------------------------------------------
-    imp_prod_loss(prod, loss,        // & ! out
-                  lsol, lrxt, lhet); // ! in
+    // -----------------------------------------------------------------------
+    //  ... form f(y)
+    // -----------------------------------------------------------------------
+    imp_prod_loss(prod, loss,        // out
+                  lsol, lrxt, lhet); // in
 
     for (int mm = 0; mm < clscnt4; ++mm) {
       forcing[mm] =
           solution[mm] * dti - (iter_invariant[mm] + prod[mm] - loss[mm]);
     } // mm
 
-    // !-----------------------------------------------------------------------
-    // ! ... solve for the mixing ratio at t(n+1)
-    // !-----------------------------------------------------------------------
+    // -----------------------------------------------------------------------
+    //  ... solve for the mixing ratio at t(n+1)
+    // -----------------------------------------------------------------------
     lu_slv(sys_jac, forcing);
     for (int mm = 0; mm < clscnt4; ++mm) {
       solution[mm] += forcing[mm];
     } // mm
 
-    // !-----------------------------------------------------------------------
-    // ! ... convergence measures
-    // !-----------------------------------------------------------------------
+    // -----------------------------------------------------------------------
+    //  ... convergence measures
+    // -----------------------------------------------------------------------
 
     if (nr_iter > 0) {
       for (int kk = 0; kk < clscnt4; ++kk) {
         int mm = permute_4[kk];
-        // printf("mm %d \n", mm);
         // BAD CONSTANT
-        if (haero::abs(solution[mm]) > 1.e-20) {
+        if (haero::abs(solution[mm]) > 1.0e-20) {
           max_delta[kk] = haero::abs(forcing[mm] / solution[mm]);
         } else {
           max_delta[kk] = zero;
@@ -115,18 +110,18 @@ void newton_raphson_iter(
 
     } // nr_iter
 
-    // !-----------------------------------------------------------------------
-    // ! ... limit iterate
-    // !-----------------------------------------------------------------------
+    // -----------------------------------------------------------------------
+    //  ... limit iterate
+    // -----------------------------------------------------------------------
     for (int kk = 0; kk < clscnt4; ++kk) {
       if (solution[kk] < zero) {
         solution[kk] = zero;
       }
     } // end kk
 
-    // !-----------------------------------------------------------------------
-    // ! ... transfer latest solution back to work array
-    // !-----------------------------------------------------------------------
+    // -----------------------------------------------------------------------
+    //  ... transfer latest solution back to work array
+    // -----------------------------------------------------------------------
 
     for (int kk = 0; kk < clscnt4; ++kk) {
       int jj = clsmap_4[kk];
@@ -134,9 +129,9 @@ void newton_raphson_iter(
       lsol[jj] = solution[mm];
     } // end kk
 
-    // !-----------------------------------------------------------------------
-    // ! ... check for convergence
-    // !-----------------------------------------------------------------------
+    // -----------------------------------------------------------------------
+    //  ... check for convergence
+    // -----------------------------------------------------------------------
 
     if (nr_iter > 0) {
       convergence = true;
@@ -144,12 +139,10 @@ void newton_raphson_iter(
         converged[kk] = true;
 
         int mm = permute_4[kk];
-        bool frc_mask = haero::abs(forcing[mm]) > small ? true : false;
+        bool frc_mask = haero::abs(forcing[mm]) > small;
         if (frc_mask) {
           converged[kk] =
-              haero::abs(forcing[mm]) <= epsilon[kk] * haero::abs(solution[mm])
-                  ? true
-                  : false;
+              haero::abs(forcing[mm]) <= epsilon[kk] * haero::abs(solution[mm]);
         } else {
           converged[kk] = true;
 
@@ -162,30 +155,27 @@ void newton_raphson_iter(
       if (convergence) {
         return;
       }
-
     } // end nr_iter
-
-  } // end nr_iter
-
+  }   // end nr_iter
 } // newton_raphson_iter
 
 KOKKOS_INLINE_FUNCTION
-void imp_sol(Real base_sol[gas_pcnst], //    ! species mixing ratios [vmr] & !
-                                       //    inout
+void imp_sol(Real base_sol[gas_pcnst], // inout - species mixing ratios [vmr]
              const Real reaction_rates[rxntot], const Real het_rates[gas_pcnst],
              const Real extfrc[extcnt], Real &delt,
              const int permute_4[gas_pcnst], const int clsmap_4[gas_pcnst],
              const bool factor[itermax], Real epsilon[clscnt4],
-             Real prod_out[clscnt4], Real loss_out[clscnt4]
-             //                   xhnm, ncol, lchnk, ltrop
-) {
+             Real prod_out[clscnt4], Real loss_out[clscnt4]) {
 
-  //     !-----------------------------------------------------------------------
-  // ! ... imp_sol advances the volumetric mixing ratio
-  // ! forward one time step via the fully implicit euler scheme.
-  // ! this source is meant for small l1 cache machines such as
-  // ! the intel pentium and itanium cpus
-  // !-----------------------------------------------------------------------
+  // ---------------------------------------------------------------------------
+  //  ... imp_sol advances the volumetric mixing ratio
+  //  forward one time step via the fully implicit euler scheme.
+  //
+  // NOTE: does anyone know what this is referring to?
+  // can probably lose it since it looks like these chips were axed in 2020/2023
+  // this source is meant for small l1 cache machines such as
+  // the intel pentium and itanium cpus
+  // ---------------------------------------------------------------------------
   const Real zero = 0;
   const Real half = 0.5;
   const Real one = 1;
@@ -200,35 +190,13 @@ void imp_sol(Real base_sol[gas_pcnst], //    ! species mixing ratios [vmr] & !
   Real prod[clscnt4] = {};
   Real loss[clscnt4] = {};
   Real max_delta[clscnt4] = {};
-  // ! initiate variables
-  // prod_out(:,:,:) = 0._r8
-  // loss_out(:,:,:) = 0._r8
-  // solution(:) = 0._r8
 
-  // !-----------------------------------------------------------------------
-  // ! ... class independent forcing
-  // !-----------------------------------------------------------------------
-  indprd(4,                     //                  & ! in
-         ind_prd,               //               & ! inout
-         reaction_rates, extfrc //   & ! in
-  );                            // ! in
-
-  // level_loop : do lev = 1,pver
-  //    column_loop : do icol = 1,ncol
-  //       if (lev <= ltrop(icol)) cycle column_loop
-
-  // !-----------------------------------------------------------------------
-  // ! ... transfer from base to local work arrays
-  // !-----------------------------------------------------------------------
-
-  // do mm = 1,rxntot
-  //   lrxt(mm) = reaction_rates(icol,lev,mm)
-  // enddo
-  // if( gas_pcnst > 0 ) then
-  //   do mm = 1,gas_pcnst
-  //     lhet(mm) = het_rates(icol,lev,mm)
-  //   enddo
-  // endif
+  // -----------------------------------------------------------------------
+  //  ... class independent forcing
+  // -----------------------------------------------------------------------
+  indprd(4,                       // in
+         ind_prd,                 // inout
+         reaction_rates, extfrc); // in
 
   Real solution[clscnt4] = {};
   Real iter_invariant[clscnt4] = {};
@@ -241,16 +209,16 @@ void imp_sol(Real base_sol[gas_pcnst], //    ! species mixing ratios [vmr] & !
   int fail_cnt = 0;
   int stp_con_cnt = 0;
   Real interval_done = zero;
-  // time_step_loop : do
+  // time_step_loop
   for (int i = 0; i < max_time_steps; ++i) {
     const Real dti = one / dt;
-    // !-----------------------------------------------------------------------
-    // ! ... transfer from base to local work arrays
-    // !-----------------------------------------------------------------------
+    // -----------------------------------------------------------------------
+    //  ... transfer from base to local work arrays
+    // -----------------------------------------------------------------------
     auto &lsol = base_sol;
-    // !-----------------------------------------------------------------------
-    // ! ... transfer from base to class array
-    // !-----------------------------------------------------------------------
+    // -----------------------------------------------------------------------
+    //  ... transfer from base to class array
+    // -----------------------------------------------------------------------
 
     for (int kk = 0; kk < clscnt4; ++kk) {
       int jj = clsmap_4[kk];
@@ -258,9 +226,9 @@ void imp_sol(Real base_sol[gas_pcnst], //    ! species mixing ratios [vmr] & !
       solution[mm] = lsol[jj];
     } // kk
 
-    // !-----------------------------------------------------------------------
-    // ! ... set the iteration invariant part of the function f(y)
-    // !-----------------------------------------------------------------------
+    // -----------------------------------------------------------------------
+    //  ... set the iteration invariant part of the function f(y)
+    // -----------------------------------------------------------------------
 
     for (int mm = 0; mm < clscnt4; ++mm) {
       iter_invariant[mm] = dti * solution[mm] + ind_prd[mm];
@@ -268,39 +236,28 @@ void imp_sol(Real base_sol[gas_pcnst], //    ! species mixing ratios [vmr] & !
     //-----------------------------------------------------------------------
     // ... the linear component
     //-----------------------------------------------------------------------
-    // linmat( lin_jac,  //         & ! out
-    //        lsol, lrxt, lhet   );// ! in
-    linmat(lin_jac,                    // ! out
+    linmat(lin_jac,                    //  out
            reaction_rates, het_rates); // in
 
-    // !=======================================================================
-    // ! the newton-raphson iteration for f(y) = 0
-    // !=======================================================================
+    // =======================================================================
+    //  the newton-raphson iteration for f(y) = 0
+    // =======================================================================
 
-    newton_raphson_iter(dti, lin_jac, reaction_rates, het_rates, // & ! in
-                        iter_invariant, //                & ! in
+    newton_raphson_iter(dti, lin_jac, reaction_rates, het_rates, // in
+                        iter_invariant,                          // in
                         factor, permute_4, clsmap_4, lsol,
-                        solution,               //              & ! inout
-                        converged, convergence, //       & ! out
-                        prod, loss, max_delta, epsilon); // ! out
+                        solution,                        // inout
+                        converged, convergence,          // out
+                        prod, loss, max_delta, epsilon); // out
 
     // -----------------------------------------------------------------------
     //  ... check for newton-raphson convergence
     // -----------------------------------------------------------------------
     if (!convergence) {
-      // !-----------------------------------------------------------------------
-      //           ! ... non-convergence
-      //           !-----------------------------------------------------------------------
-
-      // !-----------------------------------------------------------------------
-      //           ! ... non-convergence
-      //           !-----------------------------------------------------------------------
+      // -----------------------------------------------------------------------
+      //            ... non-convergence
+      // -----------------------------------------------------------------------
       fail_cnt += fail_cnt;
-      // nstep = get_nstep()
-      // FIXME:
-      // write(iulog,'('' imp_sol: Time step '',1p,e21.13,'' failed to converge
-      // @ (lchnk,lev,col,nstep) = '',4i6)') &
-      //      dt,lchnk,lev,icol,nstep
 
       stp_con_cnt = 0;
 
@@ -313,6 +270,7 @@ void imp_sol(Real base_sol[gas_pcnst], //    ! species mixing ratios [vmr] & !
         } else {
           dt *= 0.1;
         } // cut_cnt < cut_limit
+        // FIXME: is this part of the below FIXME?
         // break;
         // cycle time_step_loop
       } else {
@@ -327,8 +285,7 @@ void imp_sol(Real base_sol[gas_pcnst], //    ! species mixing ratios [vmr] & !
         //                 endif
         //              enddo
       } //  cut_cnt < cut_limit
-
-    } // non-convergence
+    }   // non-convergence
 
     // -----------------------------------------------------------------------
     // ... check for interval done
@@ -337,18 +294,16 @@ void imp_sol(Real base_sol[gas_pcnst], //    ! species mixing ratios [vmr] & !
     interval_done += dt;
 
     // BAD CONSTANT
-    if (haero::abs(delt - interval_done) <= .0001) {
+    if (haero::abs(delt - interval_done) <= 0.0001) {
       if (fail_cnt > 0) {
-        // FIXME
+        // FIXME: probably handle this more gracefully via error logging?
         printf("'imp_sol : @ (lchnk,lev,col) = \n");
-        // write(iulog,*) 'imp_sol : @ (lchnk,lev,col) = ',lchnk,lev,icol,'
-        // failed ',fail_cnt,' times'
       }
       break;
     } else {
-      //                  !-----------------------------------------------------------------------
-      // ! ... transfer latest solution back to base array
-      // !-----------------------------------------------------------------------
+      // -----------------------------------------------------------------------
+      //  ... transfer latest solution back to base array
+      // -----------------------------------------------------------------------
       if (convergence) {
         stp_con_cnt += 1;
       }
@@ -365,27 +320,24 @@ void imp_sol(Real base_sol[gas_pcnst], //    ! species mixing ratios [vmr] & !
       dt = haero::min(dt, delt - interval_done);
 
     } // abs( delt - interval_done ) <= .0001
-
-  } // time_step_loop
+  }   // time_step_loop
 
   //-----------------------------------------------------------------------
   // ... Transfer latest solution back to base array
-  //    and calculate Prod/Loss history buffers
+  //     and calculate Prod/Loss history buffers
   //-----------------------------------------------------------------------
 
   for (int kk = 0; kk < clscnt4; ++kk) {
     const int jj = clsmap_4[kk];
     const int mm = permute_4[kk];
-    // ! ... Transfer latest solution back to base array
+    //  ... Transfer latest solution back to base array
     base_sol[jj] = solution[mm];
-    // ! ... Prod/Loss history buffers...
+    //  ... Prod/Loss history buffers...
     prod_out[kk] = prod[mm] + ind_prd[mm];
     loss_out[kk] = loss[mm];
 
   } // cls_loop
-
 } // imp_sol
-
 } // namespace gas_chemistry
 } // namespace mam4
 #endif
