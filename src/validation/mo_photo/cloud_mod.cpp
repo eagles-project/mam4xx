db422dd8a2f2a0d16d399a52a1d9f286f19af55e
Oscar Diaz-Ibarra <odiazib@sandia.gov>
2023-07-28 22:15:17 -0600

    mo_photo - validation test

---
+++ src/validation/mo_photo/cloud_mod.cpp
@@ -0,0 +1,20 @@
+// mam4xx: Copyright (c) 2022,
+// Battelle Memorial Institute and
+// National Technology & Engineering Solutions of Sandia, LLC (NTESS)
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include <mam4xx/mam4.hpp>
+
+#include <mam4xx/aero_config.hpp>
+#include <skywalker.hpp>
+#include <validation.hpp>
+
+using namespace skywalker;
+using namespace mam4;
+using namespace haero;
+void cloud_mod(Ensemble *ensemble) {
+  ensemble->process([=](const Input &input, Output &output) {
+    // number of vertical points.
+    // validation test from standalone mo_photo.
+  });
+}
