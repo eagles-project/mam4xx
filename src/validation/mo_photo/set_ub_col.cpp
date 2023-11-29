e543c15a80c4a0e9d8e56ec9ac5150b22ec320ca
Jeffrey N. Johnson <jeff@cohere-llc.com>
2023-11-29 10:06:35 -0800

    Reformatted.

--- src/validation/mo_photo/set_ub_col.cpp
+++ src/validation/mo_photo/set_ub_col.cpp
@@ -30,27 +30,30 @@ void set_ub_col(Ensemble *ensemble) {
     View1D pdel("pdel", pver);
 
     mam4::validation::convert_1d_vector_to_2d_view_device(vmr_in, vmr);
-    mam4::validation::convert_1d_vector_to_2d_view_device(invariants_in, invariants);
+    mam4::validation::convert_1d_vector_to_2d_view_device(invariants_in,
+                                                          invariants);
     auto pdel_host = Kokkos::create_mirror_view(pdel);
     std::copy(pdel_in.data(), pdel_in.data() + pver, pdel_host.data());
     Kokkos::deep_copy(pdel, pdel_host);
 
-    View1D col_delta("col_delta", pver+1);
+    View1D col_delta("col_delta", pver + 1);
     col_delta(0) = spc_exo_col;
-    Kokkos::parallel_for(pver, KOKKOS_LAMBDA(const int k) {
-      Real vmr_ik[gas_pcnst];
-      for (int l = 0; l < gas_pcnst; ++l) {
-        vmr_ik[l] = vmr(k, l);
-      }
-      Real inv_ik[nfs];
-      for (int l = 0; l < nfs; ++l) {
-        inv_ik[l] = invariants(k, l);
-      }
-      mam4::mo_photo::set_ub_col(col_delta(k+1), vmr_ik, inv_ik, pdel(k));
-    });
+    Kokkos::parallel_for(
+        pver, KOKKOS_LAMBDA(const int k) {
+          Real vmr_ik[gas_pcnst];
+          for (int l = 0; l < gas_pcnst; ++l) {
+            vmr_ik[l] = vmr(k, l);
+          }
+          Real inv_ik[nfs];
+          for (int l = 0; l < nfs; ++l) {
+            inv_ik[l] = invariants(k, l);
+          }
+          mam4::mo_photo::set_ub_col(col_delta(k + 1), vmr_ik, inv_ik, pdel(k));
+        });
 
     auto col_delta_host = Kokkos::create_mirror_view(col_delta);
-    std::vector<Real> col_delta_out(col_delta_host.data(), col_delta_host.data() + pver+1);
+    std::vector<Real> col_delta_out(col_delta_host.data(),
+                                    col_delta_host.data() + pver + 1);
     output.set("col_delta", col_delta_out);
   });
 }
