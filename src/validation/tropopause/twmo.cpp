ab048639befd7b81faa9512f7578b81fb158dd7e
Jaelyn Litzinger <jaelyn.litzinger@pnnl.gov>
2023-12-07 17:01:05 -0800

    add validation and source code for twmo

---
+++ src/validation/tropopause/twmo.cpp
@@ -0,0 +1,41 @@
+// mam4xx: Copyright (c) 2022,
+// Battelle Memorial Institute and
+// National Technology & Engineering Solutions of Sandia, LLC (NTESS)
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include <mam4xx/mam4.hpp>
+
+#include <mam4xx/aero_config.hpp>
+#include <skywalker.hpp>
+#include <validation.hpp>
+
+using namespace skywalker;
+using namespace mam4;
+using namespace haero;
+
+void twmo(Ensemble *ensemble) {
+  ensemble->process([=](const Input &input, Output &output) {
+    using View1DHost = typename HostType::view_1d<Real>;
+    using ColumnView = haero::ColumnView;
+    constexpr int pver = mam4::nlev;
+
+    const auto pmid1d_in = input.get_array("pmid1d");
+    const auto temp1d_in = input.get_array("temp1d");
+    const Real plimu = input.get_array("plimu")[0];
+    const Real pliml = input.get_array("pliml")[0];
+    const Real gam = input.get_array("gam")[0];
+    Real trp = input.get_array("trp")[0];
+
+    ColumnView pmid1d, temp1d;
+    auto pmid1d_host = View1DHost((Real *)pmid1d_in.data(), pver);
+    auto temp1d_host = View1DHost((Real *)temp1d_in.data(), pver);
+    pmid1d = haero::testing::create_column_view(pver);
+    temp1d = haero::testing::create_column_view(pver);
+    Kokkos::deep_copy(pmid1d, pmid1d_host);
+    Kokkos::deep_copy(temp1d, temp1d_host);
+
+    tropopause::twmo(temp1d, pmid1d, plimu, pliml, gam, trp);
+
+    output.set("trp", trp);
+  });
+}
