0e0f22234e7819b97f1391c40cb8863d2ea53066
Jeffrey N. Johnson <jeff@cohere-llc.com>
2023-12-19 15:11:16 -0800

    Added mo_drydep validation stubs.

---
+++ src/validation/mo_drydep/drydep_xactive.cpp
@@ -0,0 +1,10 @@
+#include <mam4xx/mam4.hpp>
+
+#include <skywalker.h>
+#include <validation.hpp>
+
+using namespace skywalker;
+using namespace mam4;
+using namespace haero;
+void drydep_xactive(Ensemble *ensemble) {
+}
