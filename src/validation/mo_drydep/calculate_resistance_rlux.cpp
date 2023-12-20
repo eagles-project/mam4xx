284bb7bca726d1cf64d25842b0f8d57a535bccb4
Jeffrey N. Johnson <jeff@cohere-llc.com>
2023-12-20 14:38:50 -0800

    Finished drafts of all validation code.

--- src/validation/mo_drydep/calculate_resistance_rlux.cpp
+++ src/validation/mo_drydep/calculate_resistance_rlux.cpp
@@ -6,4 +6,66 @@
 using namespace skywalker;
 using namespace mam4;
 using namespace haero;
-void calculate_resistance_rlux(Ensemble *ensemble) {}
+void calculate_resistance_rlux(Ensemble *ensemble) {
+  ensemble->process([=](const Input &input, Output &output) {
+    using View1DHost = typename HostType::view_1d<Real>;
+    using View1D = typename DeviceType::view_1d<Real>;
+
+    using ViewInt1D = typename DeviceType::view_1d<int>;
+    using ViewInt1DHost = typename HostType::view_1d<int>;
+
+    using ViewBool1D = typename DeviceType::view_1d<bool>;
+    using ViewBool1DHost = typename HostType::view_1d<bool>;
+
+    const int beglt = int(input.get_array("beglt")[0]) - 1;
+    const int endlt = int(input.get_array("endlt")[0]) - 1;
+    const auto index_season = input.get_array("index_season");
+    const auto fr_lnduse = input.get_array("fr_lnduse");
+    const bool has_rain = static_cast<bool>(input.get_array("has_rain")[0]);
+    const bool has_dew = static_cast<bool>(input.get_array("has_dew")[0]);
+    const Real sfc_temp = input.get_array("sfc_temp")[0];
+    const auto heff = input.get_array("heff");
+    const Real cts = input.get_array("cts")[0];
+
+    Viewint1DHost index_season_h("index_season", n_land_type);
+    for (int lt = 0; lt < n_land_type; ++lt) {
+      index_season_h(lt) = int(index_season[lt]);
+    }
+    ViewInt1D index_season_d("index_season", n_land_type);
+    Kokkos::deepcopy(index_season_d, index_season_h);
+
+    ViewBool1DHost fr_lnduse_h("fr_lnduse", n_land_type);
+    for (int lt = 0; lt < n_land_type; ++lt) {
+      fr_lnduse_h(lt) = static_cast<bool>(fr_lnduse[lt]);
+    }
+    ViewBool1D fr_lnduse_d("fr_lnduse", n_land_type);
+    Kokkos::deepcopy(fr_lnduse_d, fr_lnduse_h);
+
+    View1D heff_d("heff", nddvels);
+    View1DHost heff_h((Real *)heff.data(), nddvels);
+    Kokkos::deepcopy(heff_d, heff_h);
+
+    View1D rlux_d("rlux", gas_pcnst * n_land_type);
+
+    auto team_policy = ThreadTeamPolicy(1u, 1u);
+    Kokkos::parallel_for(
+        Real rlux[gas_pcnst][n_land_type];
+        team_policy, KOKKOS_LAMBDA(const ThreadTeam &team) {
+          calculate_resistance_rlux(beglt, endlt, index_season_d.data(),
+                                    fr_lnduse_d.data(), heff_d.data(), cts,
+                                    rlux);
+          // shuffle array data into view
+          int l = 0;
+          for (int i = 0; i < gas_pcnst; ++i) {
+            for (int lt = 0; lt < n_land_type; ++lt, ++l) {
+              rlux_d(l) = rlux[i][lt];
+            }
+          }
+        });
+
+    std::vector<Real> rlux(gas_pcnst * n_land_type);
+    auto rlux_h = View1DHost((Real *)rlux.data(), gas_pcnst * n_land_type);
+    Kokkos::deep_copy(rlux_h, rlux_d);
+    output.set("rlux","rlux);
+  });
+}
