2e13c851f5bb1c052fb0a746bc571bc1e42efb36
James Overfelt <jroverf@sandia.gov>
2024-03-18 14:27:04 -0600

    Initialize the Atmosphere to avoid FPEs

--- src/tests/mam4_nucleation_unit_tests.cpp
+++ src/tests/mam4_nucleation_unit_tests.cpp
@@ -4,6 +4,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
 #include "testing.hpp"
+#include "atmosphere_utils.hpp"
 #include <mam4xx/mam4.hpp>
 
 #include <catch2/catch.hpp>
@@ -33,7 +34,15 @@ TEST_CASE("test_compute_tendencies", "mam4_nucleation_process") {
 
   int nlev = 72;
   Real pblh = 1000;
-  Atmosphere atm = mam4::testing::create_atmosphere(nlev, pblh);
+  // these values correspond to a humid atmosphere with relative humidity
+  // values approximately between 32% and 98%
+  const Real Tv0 = 300;     // reference virtual temperature [K]
+  const Real Gammav = 0.01; // virtual temperature lapse rate [K/m]
+  const Real qv0 =
+      0.015; // specific humidity at surface [kg h2o / kg moist air]
+  const Real qv1 = 7.5e-4; // specific humidity lapse rate [1 / m]
+  Atmosphere atm = mam4::init_atm_const_tv_lapse_rate(nlev, pblh, Tv0, Gammav, qv0, qv1);
+
   Surface sfc = mam4::testing::create_surface();
   mam4::Prognostics progs = mam4::testing::create_prognostics(nlev);
   mam4::Diagnostics diags = mam4::testing::create_diagnostics(nlev);
@@ -111,7 +120,15 @@ TEST_CASE("test_multicol_compute_tendencies", "mam4_nucleation_process") {
   DeviceType::view_1d<mam4::Tendencies> mc_tends("mc_tends", ncol);
   int nlev = 72;
   Real pblh = 1000;
-  Atmosphere atmosphere = mam4::testing::create_atmosphere(nlev, pblh);
+  // these values correspond to a humid atmosphere with relative humidity
+  // values approximately between 32% and 98%
+  const Real Tv0 = 300;     // reference virtual temperature [K]
+  const Real Gammav = 0.01; // virtual temperature lapse rate [K/m]
+  const Real qv0 =
+      0.015; // specific humidity at surface [kg h2o / kg moist air]
+  const Real qv1 = 7.5e-4; // specific humidity lapse rate [1 / m]
+  Atmosphere atmosphere = mam4::init_atm_const_tv_lapse_rate(nlev, pblh, Tv0, Gammav, qv0, qv1);
+
   Surface surface = mam4::testing::create_surface();
   mam4::Prognostics prognostics = mam4::testing::create_prognostics(nlev);
   mam4::Diagnostics diagnostics = mam4::testing::create_diagnostics(nlev);
