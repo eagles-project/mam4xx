17e1e30846ec4965b940f4e77c388df89c528467
Oscar Diaz-Ibarra <odiazib@sandia.gov>
2024-04-09 21:02:29 -0600

    wet_dep -clang format

--- src/validation/aero_model/aero_model_wetdep.cpp
+++ src/validation/aero_model/aero_model_wetdep.cpp
@@ -68,20 +68,21 @@ void aero_model_wetdep(Ensemble *ensemble) {
     wetdep::View1D aerdepwetis("aerdepwetis", aero_model::pcnst);
     const int num_modes = AeroConfig::num_modes();
 
-    wetdep::View2D wet_geometric_mean_diameter_i("wet_geometric_mean_diameter_i",num_modes, nlev);
+    wetdep::View2D wet_geometric_mean_diameter_i(
+        "wet_geometric_mean_diameter_i", num_modes, nlev);
     Kokkos::deep_copy(wet_geometric_mean_diameter_i, 1.0);
 
-    wetdep::View2D dry_geometric_mean_diameter_i("dry_geometric_mean_diameter_i",num_modes, nlev);
+    wetdep::View2D dry_geometric_mean_diameter_i(
+        "dry_geometric_mean_diameter_i", num_modes, nlev);
     Kokkos::deep_copy(dry_geometric_mean_diameter_i, 1.0);
 
-    wetdep::View2D qaerwat("qaerwat",num_modes, nlev);
+    wetdep::View2D qaerwat("qaerwat", num_modes, nlev);
     Kokkos::deep_copy(qaerwat, 1.0);
 
-    wetdep::View2D wetdens("wetdens",num_modes, nlev);
+    wetdep::View2D wetdens("wetdens", num_modes, nlev);
     Kokkos::deep_copy(wetdens, 1.0);
 
-    wetdep::View2D ptend_q("ptend_q",nlev, aero_model::pcnst);
-
+    wetdep::View2D ptend_q("ptend_q", nlev, aero_model::pcnst);
 
     const int work_len = wetdep::get_aero_model_wetdep_work_len();
     wetdep::View1D work("work", work_len);
@@ -94,29 +95,26 @@ void aero_model_wetdep(Ensemble *ensemble) {
         team_policy, KOKKOS_LAMBDA(const ThreadTeam &team) {
           auto progs_in = progs;
           auto tends_in = tends;
-          wetdep::aero_model_wetdep(team, atm, progs_in, tends_in, dt,
-                                    // inputs
-                                    cldt, 
-                                    cldn_prev_step, rprdsh, rprddp, evapcdp,
-                                    evapcsh, dp_frac, sh_frac,
-                                    icwmrdp, icwmrsh, evapr,
-                                    dlf,
-                                    wet_geometric_mean_diameter_i,
-                                    dry_geometric_mean_diameter_i, 
-                                    qaerwat, 
-                                    wetdens, 
-                                    // output
-                                    aerdepwetis, aerdepwetcw,
-                                    // FIXME
-                                    qqcw_sav, work);
-    
-    team.team_barrier();
-    Kokkos::parallel_for(Kokkos::TeamThreadRange(team, 0, nlev), [&](int kk) {
-    const auto ptend_q_kk = ekat::subview(ptend_q, kk);
-    utils::extract_ptend_from_tendencies(tends_in, ptend_q_kk.data(), kk);
-    });
+          wetdep::aero_model_wetdep(
+              team, atm, progs_in, tends_in, dt,
+              // inputs
+              cldt, cldn_prev_step, rprdsh, rprddp, evapcdp, evapcsh, dp_frac,
+              sh_frac, icwmrdp, icwmrsh, evapr, dlf,
+              wet_geometric_mean_diameter_i, dry_geometric_mean_diameter_i,
+              qaerwat, wetdens,
+              // output
+              aerdepwetis, aerdepwetcw,
+              // FIXME
+              qqcw_sav, work);
 
-    });
+          team.team_barrier();
+          Kokkos::parallel_for(
+              Kokkos::TeamThreadRange(team, 0, nlev), [&](int kk) {
+                const auto ptend_q_kk = ekat::subview(ptend_q, kk);
+                utils::extract_ptend_from_tendencies(tends_in,
+                                                     ptend_q_kk.data(), kk);
+              });
+        });
 
     std::vector<Real> dlf_output(nlev, 0);
     auto dlf_host = View1DHost((Real *)dlf_output.data(), nlev);
@@ -124,39 +122,37 @@ void aero_model_wetdep(Ensemble *ensemble) {
     output.set("dlf", dlf_output);
 
     std::vector<Real> output_pcnst(aero_model::pcnst, 0);
-    auto aerdepwetcw_host = View1DHost((Real *)output_pcnst.data(), aero_model::pcnst);
+    auto aerdepwetcw_host =
+        View1DHost((Real *)output_pcnst.data(), aero_model::pcnst);
     Kokkos::deep_copy(aerdepwetcw, aerdepwetcw_host);
     output.set("aerdepwetcw", output_pcnst);
 
     std::vector<Real> aerdepwetis_output(aero_model::pcnst, 0);
-    auto aerdepwetis_host = View1DHost((Real *)output_pcnst.data(), aero_model::pcnst);
+    auto aerdepwetis_host =
+        View1DHost((Real *)output_pcnst.data(), aero_model::pcnst);
     Kokkos::deep_copy(aerdepwetis, aerdepwetis_host);
     output.set("aerdepwetis", output_pcnst);
 
     std::vector<Real> output_modes(nlev * num_modes, 0);
-    mam4::validation::convert_2d_view_device_to_1d_vector(wet_geometric_mean_diameter_i,
-     output_modes);
+    mam4::validation::convert_2d_view_device_to_1d_vector(
+        wet_geometric_mean_diameter_i, output_modes);
     output.set("dgnumwet", output_modes);
 
-    mam4::validation::convert_2d_view_device_to_1d_vector(dry_geometric_mean_diameter_i,
-     output_modes);
+    mam4::validation::convert_2d_view_device_to_1d_vector(
+        dry_geometric_mean_diameter_i, output_modes);
     output.set("dgnumdry", output_modes);
 
     mam4::validation::convert_2d_view_device_to_1d_vector(qaerwat,
-     output_modes);
+                                                          output_modes);
     output.set("qaerwat", output_modes);
 
     mam4::validation::convert_2d_view_device_to_1d_vector(wetdens,
-     output_modes);
+                                                          output_modes);
     output.set("wetdens", output_modes);
 
     std::vector<Real> output_ptend(nlev * aero_model::pcnst, 0);
     mam4::validation::convert_2d_view_device_to_1d_vector(ptend_q,
-     output_ptend);
+                                                          output_ptend);
     output.set("ptend_q", output_ptend);
-
-
-
-
   });
 }
