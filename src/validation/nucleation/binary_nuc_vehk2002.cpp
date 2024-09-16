73ab1a91fd3373dfa1c1b8d4a9bf81c2f55b41fe
Michael J Schmidt <mjschm@sandia.gov>
2024-09-16 15:02:02 -0600

    binary_nuc_vehk2002() passing

--- src/validation/nucleation/binary_nuc_vehk2002.cpp
+++ src/validation/nucleation/binary_nuc_vehk2002.cpp
@@ -17,9 +17,7 @@ void binary_nuc_vehk2002(Ensemble *ensemble) {
   ensemble->process([=](const Input &input, Output &output) {
     // Ensemble parameters
     // Declare array of strings for input names
-    std::string input_arrays[] = {"life",       "temp",     "rh",
-                                  "so4vol",     "ratenucl", "rateloge",
-                                  "cnum_h2so4", "cnum_tot", "radius_cluster"};
+    std::string input_arrays[] = {"temp", "rh", "so4vol"};
 
     // Iterate over input_arrays and error if not in input
     for (std::string name : input_arrays) {
@@ -29,26 +27,22 @@ void binary_nuc_vehk2002(Ensemble *ensemble) {
       }
     }
 
-    const Real life = input.get_array("life")[0];
+    const Real temp = input.get_array("temp")[0];
+    const Real rh = input.get_array("rh")[0];
+    const Real so4vol = input.get_array("so4vol")[0];
+    Real ratenucl;
+    Real rateloge;
+    Real cnum_h2so4;
+    Real cnum_tot;
+    Real radius_cluster;
 
-    // const Real temp = input.get_array("temp")[0];
-    // const Real rh = input.get_array("rh")[0];
-    // const Real so4vol = input.get_array("so4vol")[0];
-    // Real ratenucl = input.get_array("ratenucl")[0];
-    // Real rateloge = input.get_array("rateloge")[0];
-    // Real cnum_h2so4 = input.get_array("cnum_h2so4")[0];
-    // Real cnum_tot = input.get_array("cnum_tot")[0];
-    // Real radius_cluster = input.get_array("radius_cluster")[0];
+    nucleation::binary_nuc_vehk2002(temp, rh, so4vol, ratenucl, rateloge,
+                                       cnum_h2so4, cnum_tot, radius_cluster);
 
-    // nucleation::binary_nuc_vehk2002(temp, rh, so4vol, ratenucl, rateloge,
-                                      //  cnum_h2so4, cnum_tot, radius_cluster)
-
-    output.set("ans", life);
-
-    // output.set("ratenucl", ratenucl);
-    // output.set("rateloge", rateloge);
-    // output.set("cnum_h2so4", cnum_h2so4);
-    // output.set("cnum_tot", cnum_tot);
-    // output.set("radius_cluster", radius_cluster);
+    output.set("ratenucl", ratenucl);
+    output.set("rateloge", rateloge);
+    output.set("cnum_h2so4", cnum_h2so4);
+    output.set("cnum_tot", cnum_tot);
+    output.set("radius_cluster", radius_cluster);
   });
 }
