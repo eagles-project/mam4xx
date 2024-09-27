a5affba5d36d96a035111b24dfa1ce518439a30f
James Overfelt <jroverf@sandia.gov>
2024-09-27 15:59:14 -0600

    Clang format.

--- src/mam4xx/mam4.hpp
+++ src/mam4xx/mam4.hpp
@@ -36,12 +36,12 @@
 #include <mam4xx/ndrop.hpp>
 #include <mam4xx/nucleate_ice.hpp>
 #include <mam4xx/nucleation.hpp>
+#include <mam4xx/physical_limits.hpp>
 #include <mam4xx/rename.hpp>
 #include <mam4xx/spitfire_transport.hpp>
 #include <mam4xx/tropopause.hpp>
 #include <mam4xx/water_uptake.hpp>
 #include <mam4xx/wet_dep.hpp>
-#include <mam4xx/physical_limits.hpp>
 
 namespace mam4 {
 
