56acf1e8fa31ed521e641c13da2ac936d9aec011
James Overfelt <jroverf@sandia.gov>
2024-09-27 14:32:08 -0600

    Add function to return reasonable upper and lower bounds of field values.

--- src/mam4xx/mam4.hpp
+++ src/mam4xx/mam4.hpp
@@ -41,6 +41,7 @@
 #include <mam4xx/tropopause.hpp>
 #include <mam4xx/water_uptake.hpp>
 #include <mam4xx/wet_dep.hpp>
+#include <mam4xx/physical_limits.hpp>
 
 namespace mam4 {
 
--- src/mam4xx/physical_limits.hpp
+++ src/mam4xx/physical_limits.hpp
@@ -5,24 +5,32 @@
 
 #ifndef PHYSICAL_LIMITS_HPP
 #define PHYSICAL_LIMITS_HPP
+#include <utility>
 #include <string>
 #include <map>
 
 namespace mam4 {
 
-  inline Real physical_min_max(const std::string &field_name) {
-    const std::map<std::string, std::pair<Real,Real>> 
+  inline const std::pair<Real,Real>&  physical_min_max(const std::string &field_name) {
+    static const std::map<std::string, std::pair<Real,Real>> 
       limits = {
-        {"T_min", {100,500} }
+        {"T_min", {100, 500} },
+        {"qv", {1e-13, 0.2} },
+        {"qc", {0, 0.1} },
+        {"qt", {0, 0.1} },
+        {"nc", {0, 0.1e11} },
+        {"nr", {0, 0.1e10} },
+        {"ni", {0, 0.1e10} },
+        {"nmr", {0, 0.1e13} },
+        {"mmr", {0, 0.1e-5} }
       };
-    auto iter = limits.find(field_name);
-    EKAT_REQUIRE_MSG(iter != limits.end(),
-      std::string("Bounds not defined for field name:"+field_name);
-
-    return iter->first;
+    return limits.at(field_name);
   }
 
   inline Real physical_min(const std::string &field_name) {
+    return physical_min_max(field_name).first;
+  }
+  inline Real physical_max(const std::string &field_name) {
     return physical_min_max(field_name).second;
   }
 } // namespace mam4
