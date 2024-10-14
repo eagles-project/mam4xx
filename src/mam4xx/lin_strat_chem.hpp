bd7c90e16e823c3d42fb4ff75c2e434ef9045133
Balwinder Singh <balwindersingh@gmail.com>
2024-10-14 13:08:29 -0700

    Clang format

--- src/mam4xx/lin_strat_chem.hpp
+++ src/mam4xx/lin_strat_chem.hpp
@@ -260,8 +260,8 @@ void lin_strat_chem_solve(
 
 KOKKOS_INLINE_FUNCTION
 Real lin_strat_sfcsink_kk(const Real delta_t, const Real pdel, // in
-                          Real &o3l_vmr_in, const Real o3_sfc, const Real o3_tau,
-                          Real &do3mass) {
+                          Real &o3l_vmr_in, const Real o3_sfc,
+                          const Real o3_tau, Real &do3mass) {
   constexpr Real one = 1.0;
   // BAD CONSTANT
   constexpr Real mwo3 = 48.; // molecular weight O3
@@ -273,7 +273,7 @@ Real lin_strat_sfcsink_kk(const Real delta_t, const Real pdel, // in
       (one - haero::exp(-delta_t / o3_tau));     // !compute time scale factor
                                                  //
   const Real mass = pdel * rgrav;                //   air mass in kg/m2
-  const Real o3l_old = o3l_vmr_in;                  // vmr
+  const Real o3l_old = o3l_vmr_in;               // vmr
   const Real do3 = (o3_sfc - o3l_old) * efactor; // vmr
   const Real o3l_vmr = o3l_old + do3;
   do3mass += do3 * mass * mwo3 / mwdry; // loss in kg/m2 summed over boundary
