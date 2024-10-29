4c3afc2d2127effe811ad03d2428d36bee898cde
Oscar Diaz-Ibarra <odiazib@sandia.gov>
2024-10-29 16:57:26 -0600

    Add ThreadTeam and team in cloud mode.

--- src/validation/mo_photo/cloud_mod.cpp
+++ src/validation/mo_photo/cloud_mod.cpp
@@ -15,24 +15,55 @@ using namespace haero;
 using namespace mo_photo;
 void cloud_mod(Ensemble *ensemble) {
   ensemble->process([=](const Input &input, Output &output) {
+
+    using View1DHost = typename HostType::view_1d<Real>;
+    using View1D = typename DeviceType::view_1d<Real>;
+
     // number of vertical points.
     // validation test from standalone mo_photo.
     const auto zen_angle = input.get_array("zen_angle")[0];
-    const auto clouds = input.get_array("clouds");
-    const auto lwc = input.get_array("lwc");
-    const auto delp = input.get_array("delp");
+    const auto clouds_db = input.get_array("clouds");
+    const auto lwc_db = input.get_array("lwc");
+    const auto delp_db = input.get_array("delp");
     const auto srf_alb = input.get_array("srf_alb")[0];
     // const auto  = input.get_array("");
-    const Real zero = 0;
+    constexpr Real zero = 0;
+    std::cout << "clouds_db.size() " << clouds_db.size()<< "\n";
 
-    std::vector<Real> eff_alb(pver, zero);
-    std::vector<Real> cld_mult(pver, zero);
+    auto clouds_host = View1DHost((Real *)clouds_db.data(), clouds_db.size());
+    View1D clouds("clouds", clouds_db.size());
+    Kokkos::deep_copy(clouds, clouds_host);
 
-    cloud_mod(zen_angle, clouds.data(), lwc.data(), delp.data(),
+    auto lwc_host = View1DHost((Real *)lwc_db.data(), lwc_db.size());
+    const auto lwc = View1D("lwc", lwc_db.size());
+    Kokkos::deep_copy(lwc, lwc_host);
+
+    auto delp_host = View1DHost((Real *)delp_db.data(), delp_db.size());
+    const auto delp = View1D("delp", delp_db.size());
+    Kokkos::deep_copy(delp, delp_host);
+
+    View1D eff_alb("eff_alb", pver);
+    View1D cld_mult("cld_mult", pver);
+
+    auto team_policy = ThreadTeamPolicy(1u, Kokkos::AUTO);
+#if 1
+    Kokkos::parallel_for(
+        team_policy, KOKKOS_LAMBDA(const ThreadTeam &team) {
+    cloud_mod(team, zen_angle, clouds.data(), lwc.data(), delp.data(),
               srf_alb, //  in
               eff_alb.data(), cld_mult.data());
+    });
+#endif
+    std::vector<Real> eff_alb_db(pver, zero);
+    std::vector<Real> cld_mult_db(pver, zero);
 
-    output.set("eff_alb", eff_alb);
-    output.set("cld_mult", cld_mult);
+    auto eff_alb_host = View1DHost((Real *)eff_alb_db.data(), pver);
+    auto cld_mult_host = View1DHost((Real *)cld_mult_db.data(), pver);
+    Kokkos::deep_copy(eff_alb_host, eff_alb);
+    Kokkos::deep_copy(cld_mult_host, cld_mult);
+
+
+    output.set("eff_alb", eff_alb_db);
+    output.set("cld_mult", cld_mult_db);
   });
 }
