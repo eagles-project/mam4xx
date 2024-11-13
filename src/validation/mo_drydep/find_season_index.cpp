df0e17c40efaaae53eb66b24364f3daf8792561f
Oscar Diaz-Ibarra <odiazib@sandia.gov>
2024-11-12 17:04:32 -0700

    MAM4xx: Make the find_season_index function a host function.

--- src/validation/mo_drydep/find_season_index.cpp
+++ src/validation/mo_drydep/find_season_index.cpp
@@ -14,51 +14,63 @@ using namespace haero;
 void find_season_index(Ensemble *ensemble) {
   ensemble->process([=](const Input &input, Output &output) {
     // Assuming you have a way to get these from your input
-    using View2DInt = typename DeviceType::view_2d<int>;
-    using View3DInt = typename DeviceType::view_3d<int>;
-    using View1DHost = typename HostType::view_1d<Real>;
-    using View1D = typename DeviceType::view_1d<Real>;
+    using KTH = ekat::KokkosTypes<ekat::HostDevice>;
+    using View2DIntHost = typename KTH::view_2d<int>;
+    using View3DIntHost = typename KTH::view_3d<int>;
+    using View1DHost = typename KTH::view_1d<Real>;
 
     auto clat_db = input.get_array("clat");
     auto lat_lai_db = input.get_array("lat_lai");
     auto wk_lai_db = input.get_array("wk_lai");
 
     const int plon = clat_db.size();
-    View1D clat("clat", plon);
-    auto clat_host = View1DHost((Real *)clat_db.data(), plon);
-    Kokkos::deep_copy(clat, clat_host);
+    View1DHost clat((Real *)clat_db.data(), plon);
 
     const int nlat_lai = lat_lai_db.size();
-    View1D lat_lai("lat_lai", nlat_lai);
-    auto lat_lai_host = View1DHost((Real *)lat_lai_db.data(), nlat_lai);
-    Kokkos::deep_copy(lat_lai, lat_lai_host);
+    View1DHost lat_lai((Real *)lat_lai_db.data(), nlat_lai);
 
     const int npft_lai = 11;
 
-    View3DInt wk_lai("wk_lai", nlat_lai, npft_lai, 12);
-    validation::convert_1d_vector_to_3d_view_int_device(wk_lai_db, wk_lai);
+    View3DIntHost wk_lai("wk_lai", nlat_lai, npft_lai, 12);
 
-    View2DInt index_season_lai("index_season_lai", plon, 12);
-    auto team_policy = ThreadTeamPolicy(plon, Kokkos::AUTO);
-    Kokkos::parallel_for(
-        team_policy, KOKKOS_LAMBDA(const ThreadTeam &team) {
-          const int j = team.league_rank();
-          const auto index_season_lai_at_j = ekat::subview(index_season_lai, j);
-          mo_drydep::find_season_index(clat(j), lat_lai, nlat_lai, wk_lai,
-                                       index_season_lai_at_j);
+    int count = 0;
+    for (int d3 = 0; d3 < wk_lai.extent(2); ++d3) {
+      for (int d2 = 0; d2 < wk_lai.extent(1); ++d2) {
+        for (int d1 = 0; d1 < wk_lai.extent(0); ++d1) {
+          // make sure that we are using int.
+          wk_lai(d1, d2, d3) = static_cast<int>(wk_lai_db[count]);
+          count++;
+        }
+      }
+    }
 
-          team.team_barrier();
-          // c++ to Fortran; only for validation.
-          Kokkos::parallel_for(
-              Kokkos::TeamVectorRange(team, index_season_lai_at_j.extent(0)),
-              [&](int i) {
-                index_season_lai_at_j(i) = index_season_lai_at_j(i) + 1;
-              });
-        });
+    constexpr Real r2d = 180.0 / Constants::pi; // degrees to radians
+
+    View2DIntHost index_season_lai("index_season_lai", plon, 12);
+
+    auto policy = KTH::RangePolicy(0, plon);
+    Kokkos::parallel_for(policy, [&](const int &j) {
+      const auto index_season_lai_at_j = ekat::subview(index_season_lai, j);
+      // convert to radians
+      const Real clat_rads = clat(j) * r2d;
+      mo_drydep::find_season_index(clat_rads, lat_lai, nlat_lai, wk_lai,
+                                   index_season_lai_at_j);
+      // c++ to Fortran; only for validation.
+      for (int i = 0; i < 12; ++i) {
+        index_season_lai_at_j(i) = index_season_lai_at_j(i) + 1;
+      }
+    });
 
     std::vector<Real> index_season_lai_out(plon * 12, 0.0);
-    mam4::validation::convert_2d_view_int_device_to_1d_vector(
-        index_season_lai, index_season_lai_out);
+    count = 0;
+    for (int d2 = 0; d2 < 12; ++d2) {
+      for (int d1 = 0; d1 < plon; ++d1) {
+        // make sure that we are using int.
+        index_season_lai_out[count] = index_season_lai(d1, d2);
+        count++;
+      }
+    }
+
     output.set("index_season_lai", index_season_lai_out);
   });
 }
