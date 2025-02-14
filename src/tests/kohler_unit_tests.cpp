9c5d141460c3cabddebb4cdc00f3b8328ba07c14
Jaelyn Litzinger <jaelyn.litzinger@pnnl.gov>
2025-02-14 15:44:50 -0800

    print poly components

--- src/tests/kohler_unit_tests.cpp
+++ src/tests/kohler_unit_tests.cpp
@@ -156,8 +156,8 @@ TEST_CASE("kohler_verificiation", "") {
           Kokkos::printf("loop %d, kpoly.hygroscopicity = %f\n", i, kpoly.hygroscopicity);
           Kokkos::printf("loop %d, kpoly.dry_radius_cubed = %f\n", i, kpoly.dry_radius_cubed);
           Kokkos::printf("loop %d, haero::cube(i) = %f\n", i, haero::cube(i));
-          Kokkos::printf("loop %d, (kpoly.log_rel_humidity * rwet - kpoly.kelvin_a) = %f\n", i, (kpoly.log_rel_humidity * i - kpoly.kelvin_a));
-          Kokkos::printf("loop %d, ((hygroscopicity - log_rel_humidity) * rwet + kelvin_a) = %f\n", i, ((kpoly.hygroscopicity - kpoly.log_rel_humidity) * i + kpoly.kelvin_a));
+          // Kokkos::printf("loop %d, (kpoly.log_rel_humidity * rwet - kpoly.kelvin_a) = %f\n", i, (kpoly.log_rel_humidity * i - kpoly.kelvin_a));
+          // Kokkos::printf("loop %d, ((hygroscopicity - log_rel_humidity) * rwet + kelvin_a) = %f\n", i, ((kpoly.hygroscopicity - kpoly.log_rel_humidity) * i + kpoly.kelvin_a));
           //Kokkos::printf("loop %di, rdry(i) = %f\n", i, rdry(i));
           //Kokkos::printf("loop %di, k_of_zero(i) = %f\n", i, k_of_zero(i));
           //Kokkos::printf("loop %di, k_of_rdry(i) = %f\n", i, k_of_rdry(i));
