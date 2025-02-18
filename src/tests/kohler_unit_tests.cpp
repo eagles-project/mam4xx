f612634547c757df65d5e4e4839ab77dfce32e11
Jaelyn Litzinger <jaelyn.litzinger@pnnl.gov>
2025-02-18 15:21:17 -0800

    add kelvin_a

--- src/tests/kohler_unit_tests.cpp
+++ src/tests/kohler_unit_tests.cpp
@@ -153,7 +153,7 @@ TEST_CASE("kohler_verificiation", "") {
           // kpoly.get_kpoly(0,  output);
           Kokkos::printf("in loop\n");
           Kokkos::printf("loop %d, kpoly.log_rel_humidity = %f\n", i, kpoly.log_rel_humidity);
-          //Kokkos::printf("loop %d, kpoly.kelvin_a = %f\n", i, kpoly.kelvin_a);
+          Kokkos::printf("loop %d, kpoly.kelvin_a = %f\n", i, kpoly.kelvin_a);
           Kokkos::printf("loop %d, kpoly.hygroscopicity = %f\n", i, kpoly.hygroscopicity);
           Kokkos::printf("loop %d, kpoly.dry_radius_cubed = %f\n", i, kpoly.dry_radius_cubed);
           //Kokkos::printf("loop %d, haero::cube(i) = %f\n", i, haero::cube(i));
