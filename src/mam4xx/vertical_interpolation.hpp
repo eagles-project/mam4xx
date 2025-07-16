917927a3c2746233143a4da82c19d82e222367e3
Oscar Diaz-Ibarra <odiazib@sandia.gov>
2025-07-16 14:18:54 -0600

    MAM4xx: clang-format and const variables.

--- src/mam4xx/vertical_interpolation.hpp
+++ src/mam4xx/vertical_interpolation.hpp
@@ -13,34 +13,34 @@ using View1DInt = DeviceType::view_1d<int>;
 // this version uses parallel_for for columns and levels.
 
 KOKKOS_INLINE_FUNCTION
-void vert_interp(const ThreadTeam &team, int levsiz, int pver,
+void vert_interp(const ThreadTeam &team, const int levsiz, const int pver,
                  const ConstView1D &pin, const ConstView1D &pmid,
-                 const View1D &datain, const View1D &dataout,
-                 const int unit_factor_pin=1) {
+                 const ConstView1D &datain, const View1D &dataout,
+                 const int unit_factor_pin = 1) {
   const int zero = 0;
 
   Kokkos::parallel_for(Kokkos::TeamVectorRange(team, pver), [&](int k) {
     // Top level we need to start looking is the top level for the previous k
     // for all column points
     int kupper = zero;
-    int kkstart = zero;
     // // Store level indices for interpolation
-    for (int kk = kkstart; kk < levsiz - 1; ++kk) {
-      if (pin(kk)*unit_factor_pin < pmid(k) && pmid(k) <= pin(kk + 1)*unit_factor_pin) {
+    for (int kk = 0; kk < levsiz - 1; ++kk) {
+      if (pin(kk) * unit_factor_pin < pmid(k) &&
+          pmid(k) <= pin(kk + 1) * unit_factor_pin) {
         kupper = kk;
       } // end if
     }   // end for
 
-    if (pmid(k) < pin(0)*unit_factor_pin) {
+    if (pmid(k) < pin(0) * unit_factor_pin) {
       EKAT_KERNEL_ASSERT_MSG(
           pin(0) != 0.0,
           "Error: Division by zero. The value of pin(0) is zero.\n");
-      dataout(k) = datain(0) * pmid(k) / pin(0)/unit_factor_pin;
-    } else if (pmid(k) > pin(levsiz - 1)*unit_factor_pin) {
+      dataout(k) = datain(0) * pmid(k) / pin(0) / unit_factor_pin;
+    } else if (pmid(k) > pin(levsiz - 1) * unit_factor_pin) {
       dataout(k) = datain(levsiz - 1);
     } else {
-      Real dpu = pmid(k) - pin(kupper)*unit_factor_pin;
-      Real dpl = pin(kupper + 1)*unit_factor_pin - pmid(k);
+      Real dpu = pmid(k) - pin(kupper) * unit_factor_pin;
+      Real dpl = pin(kupper + 1) * unit_factor_pin - pmid(k);
       EKAT_KERNEL_ASSERT_MSG(
           dpl + dpu != 0.0,
           "Error: Division by zero. The value of dpl + dpu is zero.\n");
