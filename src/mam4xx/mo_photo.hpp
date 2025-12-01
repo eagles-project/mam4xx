ed985da27eed543635b419b5d020972a7f8581d9
Oscar <odiazib@sandia.gov>
2025-12-01 16:06:23 -0700

    MAM4xx: Returning value directly instead of passing it as input references.

--- src/mam4xx/mo_photo.hpp
+++ src/mam4xx/mo_photo.hpp
@@ -151,8 +151,7 @@ void set_photo_table_work_arrays(const PhotoTableData &photo_table_data,
 // NOTE: (o2 is not computed--if we want to add it back to the calculation,
 // NOTE: we should properly reorganize the code before we do it)
 KOKKOS_INLINE_FUNCTION
-void set_ub_col(Real &o3_col_delta,
-                const Real vmr_o3,
+Real set_ub_col(const Real vmr_o3,
                 const Real pdel) {
   // NOTE: chemical characteristics of our current mechanism are generated in
   // NOTE: eam/src/chemistry/pp_linoz_mam4_resus_mom_soag/mo_sim_dat.F90
@@ -165,7 +164,7 @@ void set_ub_col(Real &o3_col_delta,
   // the above conditions that o3_ndx == 0, o3_inv_ndx == -1, and O2 is not
   // a species or an invariant of interest
   constexpr Real xfactor = 2.8704e21 / (9.80616 * 1.38044); // BAD_CONSTANT!
-  o3_col_delta = xfactor * pdel * vmr_o3;
+  return  xfactor * pdel * vmr_o3;
 }
 
 template <typename VectorType>
