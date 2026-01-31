236688ce8ae8b972f0dbe2ba63469ce4d2d88a5f
James Overfelt <jroverf@sandia.gov>
2026-01-31 10:18:24 -0700

    Start rewrite of compute_q_tendencies to eliminate Kokkos::single.
    Split out real values precabs and precabs_base into Kokkos 1D
    views and pre-calculate the values and pass these as constant
    scalar values for each level.
    
    The hope is that the calculation of these values outside of the
    Kokkos::single will eventually make the Kokkos::single into a
    Kokkos::parallel.
    
    Note there is still a small Kokkos::single that was not able to
    be parallelized into at Kokkos::parallel because of a recursion
    relation in the calculation of precabs and precabs_base.

--- src/validation/wetdep/wetdep_prevap.cpp
+++ src/validation/wetdep/wetdep_prevap.cpp
@@ -54,10 +54,10 @@ void test_wetdep_prevap_process(const Input &input, Output &output) {
   Kokkos::parallel_for(
       "wetdep::wetdep_prevap", 1, KOKKOS_LAMBDA(const int) {
         Real precabx_new, precabx_base_new, scavabx_new, precnumx_base_new;
-        mam4::wetdep::wetdep_prevap(
+        std::tie(precabx_new, precabx_base_new) = mam4::wetdep::wetdep_prevap(
             is_st_cu, mam_prevap_resusp_optcc, pdel_ik, pprdx, srcx, arainx,
             precabx_base_old, precabx_old, scavabx_old, precnumx_base_old,
-            precabx_new, precabx_base_new, scavabx_new, precnumx_base_new);
+            scavabx_new, precnumx_base_new);
 
         return_vals[0] = precabx_new;
         return_vals[1] = precabx_base_new;
