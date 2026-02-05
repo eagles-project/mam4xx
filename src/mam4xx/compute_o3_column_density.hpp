50075825002b5d9145e834e4a4549aea4150f25e
Oscar Diaz-Ibarra <odiazib@sandia.gov>
2026-02-05 16:11:32 -0700

    clang format

--- src/mam4xx/compute_o3_column_density.hpp
+++ src/mam4xx/compute_o3_column_density.hpp
@@ -7,8 +7,8 @@ namespace mam4 {
 
 namespace microphysics {
 
-  using View1D = DeviceType::view_1d<Real>;
-  using ConstView1D = DeviceType::view_1d<const Real>;
+using View1D = DeviceType::view_1d<Real>;
+using ConstView1D = DeviceType::view_1d<const Real>;
 
 // FIXME: check if we have ported these function in mam4xx. If no, let's move
 // them there.
@@ -78,13 +78,9 @@ void compute_o3_column_density(
                          o3_col_dens);        // out
 }
 KOKKOS_INLINE_FUNCTION
-void compute_o3_column_density(const ThreadTeam &team,
-                               const ConstView1D &pdel,
-                               const View1D &mmr_o3,
-                               const Real o3_col_deltas_0,
-                               const Real mw_o3,
-                               const View1D& o3_col_dens)
-{
+void compute_o3_column_density(const ThreadTeam &team, const ConstView1D &pdel,
+                               const View1D &mmr_o3, const Real o3_col_deltas_0,
+                               const Real mw_o3, const View1D &o3_col_dens) {
   constexpr Real xfactor = 2.8704e21 / (9.80616 * 1.38044); // BAD_CONSTANT!
   constexpr int nlev = mam4::nlev;
   Kokkos::parallel_for(Kokkos::TeamThreadRange(team, nlev), [&](int kk) {
@@ -92,11 +88,14 @@ void compute_o3_column_density(const ThreadTeam &team,
     Kokkos::parallel_reduce(
         Kokkos::ThreadVectorRange(team, kk),
         [&](int i, Real &lsum) {
-          const Real vmr_o3_i = mam4::conversions::vmr_from_mmr(mmr_o3(i), mw_o3);
+          const Real vmr_o3_i =
+              mam4::conversions::vmr_from_mmr(mmr_o3(i), mw_o3);
           lsum += xfactor * pdel(i) * vmr_o3_i;
-        }, suma);
+        },
+        suma);
     const Real vmr_o3_kk = mam4::conversions::vmr_from_mmr(mmr_o3(kk), mw_o3);
-    o3_col_dens(kk) = o3_col_deltas_0 + suma + 0.5 * xfactor * pdel(kk) * vmr_o3_kk;
+    o3_col_dens(kk) =
+        o3_col_deltas_0 + suma + 0.5 * xfactor * pdel(kk) * vmr_o3_kk;
   });
 }
 } // namespace microphysics
