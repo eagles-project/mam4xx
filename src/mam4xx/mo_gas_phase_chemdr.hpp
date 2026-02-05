b34e8d63a8e33f89193400eb4065bfb8e7c2ca40
Oscar H. Diaz-Ibarra <odiazib@sandia.gov>
2026-02-05 15:04:39 -0800

    update compute_o3_column_density

--- src/mam4xx/mo_gas_phase_chemdr.hpp
+++ src/mam4xx/mo_gas_phase_chemdr.hpp
@@ -179,6 +179,7 @@ void perform_atmospheric_chemistry_and_microphysics(
     const ConstView1D &nevapr, // nevapr evaporation [kg/kg/s] //in
     const View1D &work_set_het, const seq_drydep::Data &drydep_data,
     const MicrophysDiagnosticArrays &diag_arrays,
+    const Real o3_col_deltas_0,
     Real dvel[gas_pcnst], // deposition velocity [cm/s]
     Real dflx[gas_pcnst], mam4::Prognostics &progs) {
 
@@ -237,11 +238,15 @@ void perform_atmospheric_chemistry_and_microphysics(
                           invariants_icol,                         // out
                           atm.temperature, atm.vapor_mixing_ratio, // in
                           cnst_offline_icol, atm.pressure);        // in
-  team.team_barrier();
-  mam4::microphysics::compute_o3_column_density(team, atm, progs,      // in
-                                                // invariants_icol,       // in
-                                                adv_mass_kg_per_moles, // in
-                                                o3_col_dens_i);        // out
+
+  const int O3indx = mam4::gas_chemistry::o3_idx;
+  const auto& mmr_o3 = progs.q_gas[O3indx];
+  mam4::microphysics::compute_o3_column_density(team,
+                               atm.hydrostatic_dp, // pdel
+                               mmr_o3,
+                               o3_col_deltas_0,
+                               adv_mass_kg_per_moles[O3indx],
+                               o3_col_dens_i);
 
   // set up photolysis work arrays for this column.
   mam4::mo_photo::PhotoTableWorkArrays photo_work_arrays_icol;
