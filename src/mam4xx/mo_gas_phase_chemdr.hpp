1db23e5419fd9c8d009f150ea9b4ca426a41175e
Oscar H. Diaz-Ibarra <odiazib@sandia.gov>
2026-02-25 15:16:08 -0800

    Fix loop size of linoz computation.

--- src/mam4xx/mo_gas_phase_chemdr.hpp
+++ src/mam4xx/mo_gas_phase_chemdr.hpp
@@ -177,7 +177,9 @@ void perform_atmospheric_chemistry_and_microphysics(
     const ConstView1D
         &prain, // stratoform precip [kg/kg/s] //in precip_total_tend
     const ConstView1D &nevapr, // nevapr evaporation [kg/kg/s] //in
-    const Real o3_col_deltas_0, const View1D &work_set_het,
+    const Real o3_col_deltas_0, 
+    const ConstColumnView &zi, // in
+    const View1D &work_set_het,
     const seq_drydep::Data &drydep_data,
     const MicrophysDiagnosticArrays &diag_arrays,
     Real dvel[gas_pcnst], // deposition velocity [cm/s]
@@ -302,6 +304,13 @@ void perform_atmospheric_chemistry_and_microphysics(
         dvel,             // deposition velocity [1/cm/s]
         dflx              // deposition flux [1/cm^2/s]
     );
+  }
+  //Find tropopause (or quit simulation if not found) as extinction should be
+  // applied only above tropopause */
+  //CHECK: in optics we use pseudo_density_dry to compute zm and zi, but in microphysics we use pseudo_density.
+  int ilev_tropp=0;
+  if (linoz_conf.compute) {
+    ilev_tropp = mam4::aer_rad_props::tropopause_or_quit(atm.pressure, atm.interface_pressure, atm.temperature, atm.height, zi);
   }
   team.team_barrier();
   // compute aerosol microphysics on each vertical level within this
@@ -457,11 +466,12 @@ void perform_atmospheric_chemistry_and_microphysics(
 
       // the following things are diagnostics, which we're not
       // including in the first rev
-      Real do3_linoz = 0, do3_linoz_psc = 0, ss_o3 = 0, o3col_du_diag = 0,
+      if (kk < ilev_tropp){
+        Real do3_linoz = 0, do3_linoz_psc = 0, ss_o3 = 0, o3col_du_diag = 0,
            o3clim_linoz_diag = 0, zenith_angle_degrees = 0;
 
-      // index of "O3" in solsym array (in EAM)
-      mam4::lin_strat_chem::lin_strat_chem_solve_kk(
+        // index of "O3" in solsym array (in EAM)
+        mam4::lin_strat_chem::lin_strat_chem_solve_kk(
           // in
           o3_col_dens_i(kk), temp, zenith_angle_icol, pmid, dt, rlats,
           linoz_data.linoz_o3_clim_icol(kk), linoz_data.linoz_t_clim_icol(kk),
@@ -476,7 +486,7 @@ void perform_atmospheric_chemistry_and_microphysics(
           // outputs that are not used
           do3_linoz, do3_linoz_psc, ss_o3, o3col_du_diag, o3clim_linoz_diag,
           zenith_angle_degrees);
-
+      }
       // Update source terms above the ozone decay threshold
       if (kk >= nlev - linoz_conf.o3_lbl) {
         const Real o3l_vmr_old = vmr[o3_ndx];
