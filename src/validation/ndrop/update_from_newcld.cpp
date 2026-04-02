4d1f0e869cb010e655de957ccb08b7518f4382e5
James Overfelt <jroverf@sandia.gov>
2026-04-02 16:43:47 -0600

    Clang format.

--- src/validation/ndrop/update_from_newcld.cpp
+++ src/validation/ndrop/update_from_newcld.cpp
@@ -57,47 +57,48 @@ void update_from_newcld(Ensemble *ensemble) {
     auto nsource_col_out = input.get_array("nsource_col_out")[0];
     auto factnum_col_out = input.get_array("factnum_col_out");
 
-    View1DHost raercol_nsav_host (raercol_nsav.data(), ncnst_tot);
-    View1DHost raercol_cw_nsav_host(raercol_cw_nsav.data(),  ncnst_tot);
-    View1D raercol_nsav_view ("raercol_nsav_view", ncnst_tot);
-    View1D raercol_cw_nsav_view ("raercol_cw_nsav_view", ncnst_tot);
+    View1DHost raercol_nsav_host(raercol_nsav.data(), ncnst_tot);
+    View1DHost raercol_cw_nsav_host(raercol_cw_nsav.data(), ncnst_tot);
+    View1D raercol_nsav_view("raercol_nsav_view", ncnst_tot);
+    View1D raercol_cw_nsav_view("raercol_cw_nsav_view", ncnst_tot);
     Kokkos::deep_copy(raercol_nsav_view, raercol_nsav_host);
     Kokkos::deep_copy(raercol_cw_nsav_view, raercol_cw_nsav_host);
 
     View1DHost qcld_host(&qcld, 1);
-    View1D qcld_view ("qcld_view", 1);
-    Kokkos::deep_copy (qcld_view, qcld_host);
-    
-    View1DHost nsource_col_out_host(&nsource_col_out, 1);
-    View1D nsource_col_out_view ("nsource_col_out_view", 1);
-    Kokkos::deep_copy (nsource_col_out_view, nsource_col_out_host);
-    
-    View1DHost factnum_col_out_host(factnum_col_out.data(), ntot_amode);
-    View1D factnum_col_out_view ("factnum_col_out_view", ntot_amode);
-    Kokkos::deep_copy (factnum_col_out_view, factnum_col_out_host);
+    View1D qcld_view("qcld_view", 1);
+    Kokkos::deep_copy(qcld_view, qcld_host);
 
-    Kokkos::parallel_for("update_from_newcld", 1, KOKKOS_LAMBDA(int) {
-      update_from_newcld(cldn_col_in, cldo_col_in, dtinv, //& ! in
-                         wtke_col_in, temp_col_in, air_density,
-                         state_q.data(), //& ! in
-                         lspectype_amode, specdens_amode, spechygro,
-                         lmassptr_amode, num2vol_ratio_min_nmodes,
-                         num2vol_ratio_max_nmodes, numptr_amode, nspec_amode,
-                         exp45logsig, alogsig, aten, mam_idx, qcld_view[0],
-                         raercol_nsav_view,
-                         raercol_cw_nsav_view, //&      ! inout
-                         nsource_col_out_view[0], factnum_col_out_view.data());
-    });
+    View1DHost nsource_col_out_host(&nsource_col_out, 1);
+    View1D nsource_col_out_view("nsource_col_out_view", 1);
+    Kokkos::deep_copy(nsource_col_out_view, nsource_col_out_host);
+
+    View1DHost factnum_col_out_host(factnum_col_out.data(), ntot_amode);
+    View1D factnum_col_out_view("factnum_col_out_view", ntot_amode);
+    Kokkos::deep_copy(factnum_col_out_view, factnum_col_out_host);
+
+    Kokkos::parallel_for(
+        "update_from_newcld", 1, KOKKOS_LAMBDA(int) {
+          update_from_newcld(
+              cldn_col_in, cldo_col_in, dtinv, //& ! in
+              wtke_col_in, temp_col_in, air_density,
+              state_q.data(), //& ! in
+              lspectype_amode, specdens_amode, spechygro, lmassptr_amode,
+              num2vol_ratio_min_nmodes, num2vol_ratio_max_nmodes, numptr_amode,
+              nspec_amode, exp45logsig, alogsig, aten, mam_idx, qcld_view[0],
+              raercol_nsav_view,
+              raercol_cw_nsav_view, //&      ! inout
+              nsource_col_out_view[0], factnum_col_out_view.data());
+        });
     Kokkos::deep_copy(raercol_nsav_host, raercol_nsav_view);
     Kokkos::deep_copy(raercol_cw_nsav_host, raercol_cw_nsav_view);
     Kokkos::deep_copy(qcld_host, qcld_view);
     Kokkos::deep_copy(nsource_col_out_host, nsource_col_out_view);
     Kokkos::deep_copy(factnum_col_out_host, factnum_col_out_view);
-    for (int i=0; i<ncnst_tot; ++i)
+    for (int i = 0; i < ncnst_tot; ++i)
       raercol_nsav[i] = raercol_nsav_host[i];
-    for (int i=0; i<ncnst_tot; ++i)
+    for (int i = 0; i < ncnst_tot; ++i)
       raercol_cw_nsav[i] = raercol_cw_nsav_host[i];
-    for (int i=0; i<ntot_amode; ++i)
+    for (int i = 0; i < ntot_amode; ++i)
       factnum_col_out[i] = factnum_col_out_host[i];
     qcld = qcld_host[0];
     nsource_col_out = nsource_col_out_host[0];
