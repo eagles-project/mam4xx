f3f6d6aadea5f0007116e3b3a8e7733a7841e8c2
James Overfelt <jroverf@sandia.gov>
2026-08-11 18:05:53 -0600

    Fix entry in index array that was off by one.

--- src/mam4xx/aero_config.hpp
+++ src/mam4xx/aero_config.hpp
@@ -139,7 +139,7 @@ public:
   KOKKOS_INLINE_FUNCTION
   static constexpr int mam_idx(const int amode, const int nspec) {
     const int mam_idx[ntot_amode()][nspec_max()] = {
-        {0, 1, 3, 4, 4, 5, 6, 7},
+        {0, 1, 2, 3, 4, 5, 6, 7},
         {8, 9, 10, 11, 12, -1, -1, -1},
         {13, 14, 15, 16, 17, 18, 19, 20},
         {21, 22, 23, 24, -1, -1, -1, -1}};
