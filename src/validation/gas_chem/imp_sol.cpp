d6fc68e291af247958a61ce1a3e337e7c9884b98
Oscar H. Diaz-Ibarra <odiazib@sandia.gov>
2026-09-18 10:16:55 -0600

    fixing unit test.

--- src/validation/gas_chem/imp_sol.cpp
+++ src/validation/gas_chem/imp_sol.cpp
@@ -30,9 +30,10 @@ void imp_sol(Ensemble *ensemble) {
       factor[i] = true;
     }
 
-    imp_sol(base_sol, //    ! species mixing ratios [vmr] & !
+    ImpSolResult result;
+    imp_sol(base_sol, //    ! species mixing ratios [vmr]
             reaction_rates.data(), het_rates.data(), extfrc.data(), delt,
-            factor, epsilon, prod_out.data(), loss_out.data());
+            factor, epsilon, prod_out.data(), loss_out.data(), result);
 
     output.set("base_sol", base_sol);
   });
