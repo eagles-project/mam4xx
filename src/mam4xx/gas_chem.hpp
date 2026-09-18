0ae04dc40dba8f1d0a7ba2de2ebafb3f301df680
Oscar H. Diaz-Ibarra <odiazib@sandia.gov>
2026-09-18 08:25:41 -0600

    Fixing bug.

--- src/mam4xx/gas_chem.hpp
+++ src/mam4xx/gas_chem.hpp
@@ -289,7 +289,7 @@ KOKKOS_INLINE_FUNCTION void imp_sol_impl(
 
   const int cut_limit = 5;
 
-  result = ImpSolResult{};
+  // result is initialized by caller
   result.requested_interval = delt;
   for (int kk = 0; kk < clscnt4; ++kk) {
     prod_out[kk] = zero;
